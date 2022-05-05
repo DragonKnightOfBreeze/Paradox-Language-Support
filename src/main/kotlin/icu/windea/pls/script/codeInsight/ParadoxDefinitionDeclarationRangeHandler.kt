780863a3bbd056213c3b61747a80489bb9e749e2
DragonKnightOfBreeze <dk_breeze@qq.com>
2022-05-05 19:34:16 +0800

    0.5.4 支持显示上下文信息

---
+++ src/main/kotlin/icu/windea/pls/script/codeInsight/ParadoxDefinitionDeclarationRangeHandler.kt
@@ -0,0 +1,23 @@
+package icu.windea.pls.script.codeInsight
+
+import com.intellij.codeInsight.hint.*
+import com.intellij.openapi.util.*
+import com.intellij.refactoring.suggested.*
+import icu.windea.pls.*
+import icu.windea.pls.script.psi.*
+
+/**
+ * 定义元素的上下文信息（如：`definitionKey = {`）。
+ */
+class ParadoxDefinitionDeclarationRangeHandler :DeclarationRangeHandler<ParadoxScriptProperty>{
+	override fun getDeclarationRange(container: ParadoxScriptProperty): TextRange? {
+		if(container.definitionInfo == null) return null
+		val valueElement = container.propertyValue?.value ?: return null
+		val startOffset = container.propertyKey.startOffset
+		val endOffset = when{
+			valueElement is ParadoxScriptBlock -> valueElement.startOffset + 1 //包括"{"
+			else -> valueElement.startOffset
+		}
+		return TextRange.create(startOffset, endOffset)
+	}
+}
