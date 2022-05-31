6ecb4e051e7d37a44caa3c22025adc59a366ccc0
DragonKnightOfBreeze <dk_breeze@qq.com>
2022-05-31 14:09:07 +0800

    0.6.1 更新中

--- src/main/kotlin/icu/windea/pls/script/editor/ParadoxScriptColorProvider.kt
+++ src/main/kotlin/icu/windea/pls/script/editor/ParadoxScriptColorProvider.kt
@@ -2,22 +2,45 @@ package icu.windea.pls.script.editor
 
 import com.intellij.openapi.editor.*
 import com.intellij.psi.*
+import com.intellij.psi.util.*
+import icu.windea.pls.*
+import icu.windea.pls.script.*
 import icu.windea.pls.script.psi.*
+import icu.windea.pls.script.psi.ParadoxScriptElementTypes.*
 import java.awt.*
 
 class ParadoxScriptColorProvider : ElementColorProvider {
+	//需要将lineMaker绑定到叶子节点上
+	
 	override fun getColorFrom(element: PsiElement): Color? {
-		return when(element){
-			is ParadoxScriptColor -> element.color
-			is ParadoxScriptBlock -> element.color
-			else -> null
+		return when {
+			element.firstChild != null -> null
+			element is PsiWhiteSpace -> null
+			element.language != ParadoxScriptLanguage -> null
+			else -> {
+				val elementType = element.elementType
+				when {
+					elementType == COLOR_TOKEN -> element.parent?.castOrNull<ParadoxScriptColor>()?.color
+					elementType == LEFT_BRACE -> element.parent?.castOrNull<ParadoxScriptBlock>()?.color
+					else -> null
+				}
+			}
 		}
 	}
-
+	
 	override fun setColorTo(element: PsiElement, color: Color) {
-		when(element){
-			is ParadoxScriptColor -> element.setColor(color)
-			is ParadoxScriptBlock -> element.setColor(color)
+		when {
+			element.firstChild != null -> pass()
+			element is PsiWhiteSpace -> pass()
+			element.language != ParadoxScriptLanguage -> pass()
+			else -> {
+				val elementType = element.elementType
+				when {
+					elementType == COLOR_TOKEN -> element.parent?.castOrNull<ParadoxScriptColor>()?.setColor(color)
+					elementType == LEFT_BRACE -> element.parent?.castOrNull<ParadoxScriptBlock>()?.setColor(color)
+					else -> pass()
+				}
+			}
 		}
 	}
 }
