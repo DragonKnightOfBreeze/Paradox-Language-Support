4ade0b82c913124c3f33edac66025d9f40911a11
DragonKnigtOfBreeze <dk_breeze@qq.com>
2022-07-22 12:09:55 +0800

    0.6.7 更新shell脚本

---
+++ cwt/clone_all.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+root_path=$(cd "$(dirname "$0")"; pwd)
+
+cd $root_path
+
+# rm -rf cwtools-ck2-config
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-ck2-config && echo "git clone cwtools-ck2-config: DONE" 2>&1
+
+# rm -rf cwtools-ck3-confi
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-ck3-config && echo "git clone cwtools-ck3-config: DONE" 2>&1
+
+# rm -rf cwtools-eu4-config
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-eu4-config && echo "git clone cwtools-eu4-config: DONE" 2>&1
+
+# rm -rf cwtools-hoi4-config
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-hoi4-config && echo "git clone cwtools-hoi4-config: DONE" 2>&1
+
+# rm -rf cwtools-ir-config
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-ir-config && echo "git clone cwtools-ir-config: DONE" 2>&1
+
+# rm -rf cwtools-stellaris-config
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-stellaris-config && echo "git clone cwtools-stellaris-config: DONE" 2>&1
+
+# rm -rf cwtools-vic2-config
+git clone git@github.com:/DragonKnightOfBreeze/cwtools-vic2-config && echo "git clone cwtools-vic2-config: DONE" 2>&1
