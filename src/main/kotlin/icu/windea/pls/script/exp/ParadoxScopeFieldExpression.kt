c1f778625696297f05a57a15080b147cc6b5a372
DragonKnigtOfBreeze <dk_breeze@qq.com>
2022-11-17 22:04:10 +0800

    0.7.4 更新中

--- src/main/kotlin/icu/windea/pls/script/exp/ParadoxScopeFieldExpression.kt
+++ src/main/kotlin/icu/windea/pls/script/exp/ParadoxScopeFieldExpression.kt
@@ -1,4 +1,28 @@
 package icu.windea.pls.script.exp
 
+/**
+ * 作用域字段表达式。
+ * 
+ * 语法：
+ * 
+ * ```bnf
+ * scope_field_expression ::= scope_expression +
+ * scope_expression ::= system_scope | scope_link | scope_link_from_data
+ * system_scope ::= TOKEN //predefined by Internal Config (in script_config.pls.cwt)
+ * scope_link ::= TOKEN //predefined by CWT Config (in links.cwt)
+ * scope_link_from_data ::= prefix data_source //predefined by CWT Config (in links.cwt)
+ * prefix ::= TOKEN //e.g. "event_target:" while the link's prefix is "event_target:"
+ * data_source ::= EXPRESSION //e.g. "some_variable" while the link's data source is "value[variable]"
+ * ```
+ * 
+ * 示例：
+ * 
+ * ```
+ * root
+ * root.owner
+ * event_target:some_target
+ * ```
+ */
 class ParadoxScopeFieldExpression {
+	
 }
