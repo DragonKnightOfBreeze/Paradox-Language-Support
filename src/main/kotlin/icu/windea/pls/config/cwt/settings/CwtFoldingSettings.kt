4ddeb7a78d1f80ba27d0b7cf54eeae0344355d72
DragonKnightOfBreeze <dk_breeze@qq.com>
2022-11-28 01:53:36 +0800

    0.7.6 实现代码折叠：对于变量操作表达式 更新中

---
+++ src/main/kotlin/icu/windea/pls/config/cwt/settings/CwtFoldingSettings.kt
@@ -0,0 +1,13 @@
+package icu.windea.pls.config.cwt.settings
+
+import com.intellij.psi.*
+import icu.windea.pls.config.cwt.config.*
+import icu.windea.pls.cwt.psi.*
+
+class CwtFoldingSettings(
+	override val pointer: SmartPsiElementPointer<CwtProperty>,
+	override val info: CwtConfigInfo,
+	val key: String?,
+	val keys: Set<String>?,
+	val placeholder: String
+) : CwtConfig<CwtProperty>
--- src/main/kotlin/icu/windea/pls/core/editor/ParadoxVariableOperationExpressionFoldingBuilder.kt
+++ src/main/kotlin/icu/windea/pls/core/editor/ParadoxVariableOperationExpressionFoldingBuilder.kt
@@ -2,18 +2,21 @@ package icu.windea.pls.core.editor
 
 import com.intellij.lang.*
 import com.intellij.lang.folding.*
+import com.intellij.openapi.components.*
 import com.intellij.openapi.editor.*
+import com.intellij.psi.*
+import icu.windea.pls.core.settings.*
 
-class ParadoxVariableOperationExpressionFoldingBuilder: FoldingBuilder {
+class ParadoxVariableOperationExpressionFoldingBuilder: FoldingBuilderEx() {
 	override fun getPlaceholderText(node: ASTNode): String? {
-		TODO("Not yet implemented")
+		return null
 	}
 	
 	override fun isCollapsedByDefault(node: ASTNode): Boolean {
-		TODO("Not yet implemented")
+		return service<ParadoxFoldingSettings>().collapseVariableOperationExpressions
 	}
 	
-	override fun buildFoldRegions(node: ASTNode, document: Document): Array<FoldingDescriptor> {
+	override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
 		TODO("Not yet implemented")
 	}
 }
