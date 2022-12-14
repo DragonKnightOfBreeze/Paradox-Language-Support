6454a031175c88f021cdcd98ba84b27daf9231f0
DragonKnightOfBreeze <dk_breeze@qq.com>
2022-12-15 00:07:56 +0800

    0.7.8 更新中

--- src/main/kotlin/icu/windea/pls/cwt/codeInsight/editorActions/smartEnter/CwtSmartEnterProcessor.kt
+++ src/main/kotlin/icu/windea/pls/cwt/codeInsight/editorActions/smartEnter/CwtSmartEnterProcessor.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.SmartEnterProcessorWithFixers
 import com.intellij.openapi.editor.*
 import com.intellij.psi.*
 import com.intellij.psi.util.*
+import icu.windea.pls.core.*
 import icu.windea.pls.cwt.codeStyle.*
 import icu.windea.pls.cwt.psi.*
 
@@ -15,13 +16,18 @@ class CwtSmartEnterProcessor: SmartEnterProcessorWithFixers() {
 	
 	class EqualSignFixer: Fixer<CwtSmartEnterProcessor>() {
 		override fun apply(editor: Editor, processor: CwtSmartEnterProcessor, element: PsiElement) {
-			//要求位于单独的（在文件顶部或者块中，或者选项注释中的单独的）
-			if(editor.caretModel.offset != element.textRange.endOffset) return
+			//要求光标位于行尾（忽略空白），且位于单独的（在文件顶部或者块中，或者选项注释中的单独的）的末尾（忽略空白）
+			val caretOffset = editor.caretModel.offset
+			if(!editor.document.isAtLineEnd(caretOffset)) return
 			val targetElement = element
 				.parentOfTypes(CwtString::class)
 				?.takeIf { it.parent is CwtBlockElement }
 				//?.takeIf { it.isBlockValue() }
 				?: return
+			val endOffset = element.textRange.endOffset
+			if(caretOffset != endOffset){
+				editor.document.deleteString(endOffset, caretOffset)
+			}
 			val customSettings = CodeStyle.getCustomSettings(element.containingFile, CwtCodeStyleSettings::class.java)
 			val spaceAroundSeparator = when {
 				targetElement.isBlockValue() -> customSettings.SPACE_AROUND_PROPERTY_SEPARATOR
--- src/main/kotlin/icu/windea/pls/script/codeInsight/editorActions/smartEnter/ParadoxScriptSmartEnterProcessor.kt
+++ src/main/kotlin/icu/windea/pls/script/codeInsight/editorActions/smartEnter/ParadoxScriptSmartEnterProcessor.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.*
 import com.intellij.openapi.editor.*
 import com.intellij.psi.*
 import com.intellij.psi.util.*
+import icu.windea.pls.core.*
 import icu.windea.pls.script.codeStyle.*
 import icu.windea.pls.script.psi.*
 
@@ -23,8 +24,9 @@ class ParadoxScriptSmartEnterProcessor: SmartEnterProcessorWithFixers() {
 	 */
 	class EqualSignFixer: Fixer<ParadoxScriptSmartEnterProcessor>() {
 		override fun apply(editor: Editor, processor: ParadoxScriptSmartEnterProcessor, element: PsiElement) {
-			//要求位于单独的（在文件顶部或者块中）字符串或者封装变量引用的末尾
-			if(editor.caretModel.offset != element.textRange.endOffset) return
+			//要求光标位于行尾（忽略空白），且位于单独的（在文件顶部或者块中）字符串或者封装变量引用的末尾（忽略空白）
+			val caretOffset = editor.caretModel.offset
+			if(!editor.document.isAtLineEnd(caretOffset)) return
 			val targetElement = element
 				.parentOfTypes(ParadoxScriptString::class, ParadoxScriptScriptedVariableReference::class, withSelf = false)
 				?.takeIf { it.isBlockValue() }
@@ -34,6 +36,10 @@ class ParadoxScriptSmartEnterProcessor: SmartEnterProcessorWithFixers() {
 				is ParadoxScriptString -> customSettings.SPACE_AROUND_PROPERTY_SEPARATOR
 				is ParadoxScriptScriptedVariableReference -> customSettings.SPACE_AROUND_SCRIPTED_VARIABLE_SEPARATOR
 				else -> return
+			}
+			val endOffset = element.textRange.endOffset
+			if(caretOffset != endOffset){
+				editor.document.deleteString(endOffset, caretOffset)
 			}
 			if(spaceAroundSeparator) {
 				EditorModificationUtil.insertStringAtCaret(editor, " = ")
