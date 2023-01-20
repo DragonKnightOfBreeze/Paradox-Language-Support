8a97217793aae0bd91e0fb4d32e8351564723891
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-01-20 21:50:05 +0800

    0.7.11 更新中

--- src/main/kotlin/icu/windea/pls/tool/cwt/CwtModifierConfigGenerator.kt
+++ src/main/kotlin/icu/windea/pls/tool/cwt/CwtModifierConfigGenerator.kt
@@ -1,20 +1,12 @@
 package icu.windea.pls.tool.cwt
 
-import com.intellij.openapi.application.*
-import com.intellij.openapi.fileEditor.*
 import com.intellij.openapi.project.*
-import com.intellij.openapi.util.*
-import com.intellij.openapi.vfs.*
-import com.intellij.openapi.vfs.newvfs.impl.*
-import com.intellij.psi.*
 import icu.windea.pls.config.core.config.*
 import icu.windea.pls.core.*
-import icu.windea.pls.cwt.*
-import icu.windea.pls.cwt.psi.*
 import java.io.*
 
 /**
- * 用于从`modifiers.log`生成`modifiers.cwt`，并更新`modifier_categories.cwt`
+ * 用于从`modifiers.log`生成`modifiers.cwt`和`modifier_categories.cwt`
  */
 class CwtModifierConfigGenerator(
 	val project: Project,
@@ -29,11 +21,6 @@ class CwtModifierConfigGenerator(
 	)
 	
 	fun generate() {
-		val newDisposable = Disposer.newDisposable()
-		if(ApplicationManager.getApplication().isUnitTestMode) {
-			VfsRootAccess.allowRootAccess(newDisposable, File("").absolutePath)
-		}
-		
 		val modifiersLogFile = File(modifiersLogPath)
 		val modifiersCwtFile = File(modifiersCwtPath)
 		val modifierCategoriesFile = File(modifierCategoriesPath)
@@ -54,7 +41,7 @@ class CwtModifierConfigGenerator(
 			append("modifiers = {").appendLine()
 			val categorySize = infos.maxOfOrNull { it.categories.size } ?: 0
 			infos.forEach { (name, categories) ->
-				append("    ")
+				append("\t")
 				append(name)
 				append(" = ")
 				if(categorySize == 1) {
@@ -70,48 +57,10 @@ class CwtModifierConfigGenerator(
 		}
 		modifiersCwtFile.writeText(text)
 		
-		val allCategories = infos.flatMapTo(mutableSetOf()) { it.categories }
-		
-		if(!modifierCategoriesFile.exists()) {
-			modifierCategoriesFile.writeText("modifier_categories = {}")
+		val modifierCategories = infos.flatMapTo(mutableSetOf()) { it.categories }
+		println("Modifier categories:")
+		for(modifierCategory in modifierCategories) {
+			println("- $modifierCategory")
 		}
-		
-		val fileDocumentManager = FileDocumentManager.getInstance()
-		val psiDocumentManager = PsiDocumentManager.getInstance(project)
-		
-		val modifierCategoriesVirtualFile = VfsUtil.findFileByIoFile(modifierCategoriesFile, true)!!
-		val modifierCategoriesDocument = fileDocumentManager.getDocument(modifierCategoriesVirtualFile)!!
-		val modifierCategoriesPsiFile = modifierCategoriesVirtualFile.toPsiFile<CwtFile>(project)!!
-		val rootElement = modifierCategoriesPsiFile
-			.findChild<CwtRootBlock>()!!
-			.findChildOfType<CwtProperty> { it.name == "modifier_categories" }!!
-		val categoryElements = rootElement
-			.findChild<CwtBlock>()!!
-			.findChildrenOfType<CwtProperty>()
-		val categories = categoryElements.map { it.name }.toSet()
-		categoryElements.forEach { categoryElement ->
-			if(categoryElement.name !in allCategories) categoryElement.delete()
-		}
-		val newCategories = allCategories.toMutableSet()
-		newCategories.removeAll(categories)
-		if(newCategories.isNotEmpty()) {
-			val textToInsert = newCategories.joinToString("\n") {
-				"${it.quoteIfNecessary()} = {\nsupported_scopes = { any }\n}"
-			}
-			val newRootBlock = PsiFileFactory.getInstance(project).createFileFromText(CwtLanguage, textToInsert)
-				.cast<CwtFile>()
-				.findChild<CwtRootBlock>()!!
-			val author = rootElement
-				.findChild<CwtBlock>()!!
-				.findChildOfType<CwtProperty>(false)
-				?: rootElement.findChild<CwtBlock>()!!.findChild(CwtElementTypes.LEFT_BRACE)
-			rootElement
-				.findChild<CwtBlock>()!!
-				.addRangeAfter(newRootBlock.firstChild, newRootBlock.lastChild, author)
-			modifierCategoriesPsiFile.reformatted()
-		}
-		
-		psiDocumentManager.doPostponedOperationsAndUnblockDocument(modifierCategoriesDocument)
-		newDisposable.dispose()
 	}
 }
