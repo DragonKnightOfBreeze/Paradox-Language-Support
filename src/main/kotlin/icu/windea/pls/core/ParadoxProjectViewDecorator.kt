ada3f93415ade35f426e08b3a2c8360619d1fdd0
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-02-14 16:16:14 +0800

    0.8.1 在项目视图中为模组根目录显示模组的名称和版本信息

---
+++ src/main/kotlin/icu/windea/pls/core/ParadoxProjectViewDecorator.kt
@@ -0,0 +1,27 @@
+package icu.windea.pls.core
+
+import com.intellij.ide.projectView.*
+import com.intellij.ide.projectView.impl.nodes.*
+import icu.windea.pls.*
+import icu.windea.pls.lang.model.*
+
+class ParadoxProjectViewDecorator: ProjectViewNodeDecorator {
+    override fun decorate(node: ProjectViewNode<*>, data: PresentationData) {
+        if(node is PsiFileNode) {
+            val file = node.virtualFile ?: return
+            val fileInfo = file.fileInfo ?: return
+            val rootInfo = fileInfo.rootInfo
+            if(rootInfo is ParadoxModRootInfo && file == rootInfo.rootFile) {
+                //在项目视图中为模组根目录显示模组的名称和版本信息（忽略存在locationString的情况，如项目根目录）
+                if(data.locationString != null) return
+                val name = rootInfo.descriptorInfo.name
+                val version = rootInfo.descriptorInfo.version
+                val text = buildString { 
+                    append(name)
+                    if(version != null) append("@").append(version)
+                }
+                data.locationString = text
+            }
+        }
+    }
+}
