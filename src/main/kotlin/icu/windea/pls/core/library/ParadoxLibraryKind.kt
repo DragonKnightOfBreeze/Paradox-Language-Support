610149ebc204c84fc1ae1b8e571012a0ad605cfa
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-02-16 10:42:50 +0800

    0.8.1 移除旧的库相关代码

--- src/main/kotlin/icu/windea/pls/core/library/ParadoxLibraryKind.kt
+++
@@ -1,7 +0,0 @@
-package icu.windea.pls.core.library
-
-import com.intellij.openapi.roots.libraries.*
-
-object ParadoxLibraryKind: PersistentLibraryKind<ParadoxLibraryProperties>("paradox") {
-	override fun createDefaultProperties() = ParadoxLibraryProperties
-}
--- src/main/kotlin/icu/windea/pls/core/library/ParadoxLibraryProperties.kt
+++
@@ -1,5 +0,0 @@
-package icu.windea.pls.core.library
-
-import com.intellij.openapi.roots.libraries.*
-
-object ParadoxLibraryProperties: DummyLibraryProperties()
