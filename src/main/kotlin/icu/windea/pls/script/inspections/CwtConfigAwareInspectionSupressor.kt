48d70c37fd40e2b5aac5f4e78c498d9a012c4bb9
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-03-14 23:02:12 +0800

    0.9.1 BUG修复与代码优化

---
+++ src/main/kotlin/icu/windea/pls/script/inspections/CwtConfigAwareInspectionSupressor.kt
@@ -0,0 +1,59 @@
+package icu.windea.pls.script.inspections
+
+import com.intellij.codeInspection.*
+import com.intellij.psi.*
+import icu.windea.pls.*
+import icu.windea.pls.config.*
+import icu.windea.pls.config.config.*
+import icu.windea.pls.core.*
+import icu.windea.pls.lang.*
+import icu.windea.pls.script.psi.*
+
+/**
+ * 基于CWT规则过滤代码检查。
+ */
+class CwtConfigAwareInspectionSupressor : InspectionSuppressor {
+    //UnresolvedExpressionInspection - 从定义级别向下检查
+    //<others> - 一般从成员级别或表达式级别直接检查，需要一定的兼容性处理
+    
+    override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
+        val definitionInfo = element.castOrNull<ParadoxScriptDefinitionElement>()?.definitionInfo
+        if(definitionInfo != null) {
+            if(definitionInfo.typeConfig.config.isSuppressed(toolId)) return true
+            if(definitionInfo.subtypeConfigs.any { it.config.isSuppressed(toolId) }) return true
+        }
+        
+        val configs = ParadoxConfigHandler.getConfigs(element, allowDefinition = true)
+        if(configs.isNotEmpty()) {
+            for(config in configs) {
+                //检查对应的规则
+                val configToUse = when {
+                    config is CwtValueConfig && config.propertyConfig != null -> config.propertyConfig
+                    else -> config
+                }
+                if(configToUse.isSuppressed(toolId)) return true
+                //向上检查对应的规则的所有父规则，直到不存在或者是内联的父规则为止
+                configToUse.processParent { c ->
+                    if(c is CwtPropertyConfig && c.inlineableConfig != null) {
+                        false
+                    } else {
+                        if(c.isSuppressed(toolId)) return true
+                        true
+                    }
+                }
+            }
+        }
+        
+        return false
+    }
+    
+    private fun CwtDataConfig<*>.isSuppressed(toolId: String): Boolean {
+        return options?.any {
+            it.key == "suppress" && it.stringValue == toolId
+        } ?: false
+    }
+    
+    override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> {
+        return SuppressQuickFix.EMPTY_ARRAY
+    }
+}
--- src/main/kotlin/icu/windea/pls/script/inspections/general/UnresolvedExpressionInspection.kt
+++ src/main/kotlin/icu/windea/pls/script/inspections/general/UnresolvedExpressionInspection.kt
@@ -8,6 +8,7 @@ import icu.windea.pls.*
 import icu.windea.pls.config.config.*
 import icu.windea.pls.core.collections.*
 import icu.windea.pls.lang.*
+import icu.windea.pls.lang.model.*
 import icu.windea.pls.script.psi.*
 import javax.swing.*
 
@@ -20,98 +21,110 @@ class UnresolvedExpressionInspection : LocalInspectionTool() {
     @JvmField var checkPropertyValue = true
     @JvmField var checkValue = true
     
-    //目前直接在文件级别检查，因为某些地方可能需要逃过检查
+    //从定义级别开始向下检查
     
-    override fun checkFile(file: PsiFile, manager: InspectionManager, isOnTheFly: Boolean): Array<ProblemDescriptor>? {
-        if(file !is ParadoxScriptFile) return null
-        val holder = ProblemsHolder(manager, file, isOnTheFly)
-        file.accept(object : PsiRecursiveElementWalkingVisitor() {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : PsiElementVisitor() {
             override fun visitElement(element: PsiElement) {
-                val result = when(element) {
-                    is ParadoxScriptProperty -> visitProperty(element)
-                    is ParadoxScriptValue -> visitValue(element)
-                    else -> true
-                }
-                if(result && element.isExpressionOrMemberContext()) super.visitElement(element)
-            }
-            
-            private fun visitProperty(element: ParadoxScriptProperty): Boolean {
                 ProgressManager.checkCanceled()
-                val shouldCheck = checkPropertyKey
-                if(!shouldCheck) return true
-                //skip checking property if property key may contain parameters
-                if(element.propertyKey.isParameterAwareExpression()) return false
-                val definitionMemberInfo = element.definitionMemberInfo
-                if(definitionMemberInfo == null || definitionMemberInfo.isDefinition) return true
-                val matchType = CwtConfigMatchType.INSPECTION
-                val configs = ParadoxConfigHandler.getPropertyConfigs(element, matchType = matchType)
-                val config = configs.firstOrNull()
-                if(config == null) {
-                    //这里使用合并后的子规则，即使parentProperty可以精确匹配
-                    val expect = if(showExpectInfo) {
-                        val allConfigs = element.findParentProperty()?.definitionMemberInfo?.getChildConfigs()
-                        val allExpressions = if(allConfigs.isNullOrEmpty()) emptySet() else {
-                            buildSet {
-                                for(c in allConfigs) {
-                                    if(c is CwtPropertyConfig) add(c.expression)
-                                }
-                            }
-                        }
-                        allExpressions.takeIfNotEmpty()?.joinToString()
-                    } else null
-                    val message = when {
-                        expect == null -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.1.1", element.expression)
-                        expect.isNotEmpty() -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.1.2", element.expression, expect)
-                        else -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.1.3", element.expression)
+                if(element is ParadoxScriptDefinitionElement) {
+                    val definitionInfo = element.definitionInfo
+                    if(definitionInfo != null) {
+                        visitDefinition(element, definitionInfo)
                     }
-                    holder.registerProblem(element, message)
-                    return false
                 }
-                return true
             }
-            
-            private fun visitValue(element: ParadoxScriptValue): Boolean {
-                ProgressManager.checkCanceled()
-                val shouldCheck = when {
-                    //also check if element is a scripted_variable
-                    //element is ParadoxScriptedVariableReference -> return false
-                    element.isPropertyValue() -> checkPropertyValue
-                    element.isBlockValue() -> checkValue
-                    else -> return false //skip
-                }
-                if(!shouldCheck) return true
-                //skip checking value if it may contain parameters
-                if(element is ParadoxScriptString && element.isParameterAwareExpression()) return false
-                val definitionMemberInfo = element.definitionMemberInfo
-                if(definitionMemberInfo == null || definitionMemberInfo.isDefinition) return true
-                val matchType = CwtConfigMatchType.INSPECTION
-                val configs = ParadoxConfigHandler.getValueConfigs(element, matchType = matchType, orDefault = false)
-                val config = configs.firstOrNull()
-                if(config == null) {
-                    val expect = if(showExpectInfo) {
-                        val allConfigs = ParadoxConfigHandler.getValueConfigs(element, orDefault = true)
-                        val allExpressions = if(allConfigs.isEmpty()) emptySet() else {
-                            buildSet {
-                                for(c in allConfigs) {
-                                    add(c.expression)
-                                }
-                            }
+    
+            @Suppress("UNUSED_PARAMETER")
+            private fun visitDefinition(element: ParadoxScriptDefinitionElement, definitionInfo: ParadoxDefinitionInfo) {
+                element.accept(object : PsiRecursiveElementWalkingVisitor() {
+                    override fun visitElement(element: PsiElement) {
+                        val result = when(element) {
+                            is ParadoxScriptProperty -> visitProperty(element)
+                            is ParadoxScriptValue -> visitValue(element)
+                            else -> true
                         }
-                        allExpressions.takeIfNotEmpty()?.joinToString()
-                    } else null
-                    val message = when {
-                        expect == null -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.2.1", element.expression)
-                        expect.isNotEmpty() -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.2.2", element.expression, expect)
-                        else -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.2.3", element.expression)
+                        if(result && element.isExpressionOrMemberContext()) super.visitElement(element)
                     }
-                    holder.registerProblem(element, message)
-                    //skip checking children
-                    return false
-                }
-                return true
+    
+                    private fun visitProperty(element: ParadoxScriptProperty): Boolean {
+                        ProgressManager.checkCanceled()
+                        val shouldCheck = checkPropertyKey
+                        if(!shouldCheck) return true
+                        //skip checking property if property key may contain parameters
+                        if(element.propertyKey.isParameterAwareExpression()) return false
+                        val definitionMemberInfo = element.definitionMemberInfo
+                        if(definitionMemberInfo == null || definitionMemberInfo.isDefinition) return true
+                        val matchType = CwtConfigMatchType.INSPECTION
+                        val configs = ParadoxConfigHandler.getPropertyConfigs(element, matchType = matchType)
+                        val config = configs.firstOrNull()
+                        if(config == null) {
+                            //这里使用合并后的子规则，即使parentProperty可以精确匹配
+                            val expect = if(showExpectInfo) {
+                                val allConfigs = element.findParentProperty()?.definitionMemberInfo?.getChildConfigs()
+                                val allExpressions = if(allConfigs.isNullOrEmpty()) emptySet() else {
+                                    buildSet {
+                                        for(c in allConfigs) {
+                                            if(c is CwtPropertyConfig) add(c.expression)
+                                        }
+                                    }
+                                }
+                                allExpressions.takeIfNotEmpty()?.joinToString()
+                            } else null
+                            val message = when {
+                                expect == null -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.1.1", element.expression)
+                                expect.isNotEmpty() -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.1.2", element.expression, expect)
+                                else -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.1.3", element.expression)
+                            }
+                            holder.registerProblem(element, message)
+                            return false
+                        }
+                        return true
+                    }
+    
+                    private fun visitValue(element: ParadoxScriptValue): Boolean {
+                        ProgressManager.checkCanceled()
+                        val shouldCheck = when {
+                            //also check if element is a scripted_variable
+                            //element is ParadoxScriptedVariableReference -> return false
+                            element.isPropertyValue() -> checkPropertyValue
+                            element.isBlockValue() -> checkValue
+                            else -> return false //skip
+                        }
+                        if(!shouldCheck) return true
+                        //skip checking value if it may contain parameters
+                        if(element is ParadoxScriptString && element.isParameterAwareExpression()) return false
+                        val definitionMemberInfo = element.definitionMemberInfo
+                        if(definitionMemberInfo == null || definitionMemberInfo.isDefinition) return true
+                        val matchType = CwtConfigMatchType.INSPECTION
+                        val configs = ParadoxConfigHandler.getValueConfigs(element, matchType = matchType, orDefault = false)
+                        val config = configs.firstOrNull()
+                        if(config == null) {
+                            val expect = if(showExpectInfo) {
+                                val allConfigs = ParadoxConfigHandler.getValueConfigs(element, orDefault = true)
+                                val allExpressions = if(allConfigs.isEmpty()) emptySet() else {
+                                    buildSet {
+                                        for(c in allConfigs) {
+                                            add(c.expression)
+                                        }
+                                    }
+                                }
+                                allExpressions.takeIfNotEmpty()?.joinToString()
+                            } else null
+                            val message = when {
+                                expect == null -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.2.1", element.expression)
+                                expect.isNotEmpty() -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.2.2", element.expression, expect)
+                                else -> PlsBundle.message("inspection.script.general.unresolvedExpression.description.2.3", element.expression)
+                            }
+                            holder.registerProblem(element, message)
+                            //skip checking children
+                            return false
+                        }
+                        return true
+                    }
+                })
             }
-        })
-        return holder.resultsArray
+        }
     }
     
     override fun createOptionsPanel(): JComponent {
