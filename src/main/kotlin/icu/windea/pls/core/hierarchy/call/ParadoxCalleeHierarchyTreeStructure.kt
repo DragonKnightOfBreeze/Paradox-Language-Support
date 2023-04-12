cc3e6bd6ace4034064b22c11adcdef6ee4ce0678
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-04-12 23:38:45 +0800

    0.9.9 实现调用层级（`Navigate > Call Hierarchy`） - 用于基于指定定义列出它所调用或者调用它的那些定义/封装变量（可以递归列出） - 限定作用域类型

--- src/main/kotlin/icu/windea/pls/core/hierarchy/call/ParadoxCalleeHierarchyTreeStructure.kt
+++ src/main/kotlin/icu/windea/pls/core/hierarchy/call/ParadoxCalleeHierarchyTreeStructure.kt
@@ -4,7 +4,9 @@ import com.intellij.ide.hierarchy.*
 import com.intellij.openapi.project.*
 import com.intellij.psi.*
 import icu.windea.pls.*
+import icu.windea.pls.core.hierarchy.type.*
 import icu.windea.pls.core.psi.*
+import icu.windea.pls.core.search.scope.type.*
 import icu.windea.pls.lang.inline.*
 import icu.windea.pls.script.psi.*
 import java.util.*
@@ -27,6 +29,8 @@ class ParadoxCalleeHierarchyTreeStructure(
     }
     
     private fun processElement(element: PsiElement, descriptor: HierarchyNodeDescriptor, descriptors: MutableList<ParadoxCallHierarchyNodeDescriptor>) {
+        val scopeType = getScopeType()
+        val scope = ParadoxSearchScopeTypes.get(scopeType).getGlobalSearchScope(myProject, element)
         element.acceptChildren(object : PsiRecursiveElementWalkingVisitor() {
             override fun visitElement(element: PsiElement) {
                 //兼容向下内联的情况
@@ -50,14 +54,22 @@ class ParadoxCalleeHierarchyTreeStructure(
                 val resolved = element.reference?.resolve() as? ParadoxScriptDefinitionElement
                 if(resolved == null) return
                 if(resolved.definitionInfo == null) return
+                val resolvedFile = selectFile(resolved)
+                if(resolvedFile != null && scope != null && !scope.contains(resolvedFile)) return
                 descriptors.add(ParadoxCallHierarchyNodeDescriptor(myProject, descriptor, resolved, false))
             }
             
             private fun addDescriptor(element: ParadoxScriptedVariableReference) {
                 val resolved = element.reference.resolve()
                 if(resolved == null) return
+                val resolvedFile = selectFile(resolved)
+                if(resolvedFile != null && scope != null && !scope.contains(resolvedFile)) return
                 descriptors.add(ParadoxCallHierarchyNodeDescriptor(myProject, descriptor, resolved, false))
             }
         })
+    }
+    
+    private fun getScopeType(): String {
+        return ParadoxHierarchyBrowserSettings.getInstance(myProject).scopeTypes.get(ParadoxHierarchyBrowserSettings.CALL) ?: "all"
     }
 }
--- src/main/kotlin/icu/windea/pls/core/hierarchy/call/ParadoxCallerHierarchyTreeStructure.kt
+++ src/main/kotlin/icu/windea/pls/core/hierarchy/call/ParadoxCallerHierarchyTreeStructure.kt
@@ -3,8 +3,11 @@ package icu.windea.pls.core.hierarchy.call
 import com.intellij.ide.hierarchy.*
 import com.intellij.openapi.project.*
 import com.intellij.psi.*
+import com.intellij.psi.search.*
 import com.intellij.psi.search.searches.*
 import icu.windea.pls.core.*
+import icu.windea.pls.core.hierarchy.type.*
+import icu.windea.pls.core.search.scope.type.*
 import icu.windea.pls.script.*
 import icu.windea.pls.script.psi.*
 
@@ -16,10 +19,12 @@ class ParadoxCallerHierarchyTreeStructure(
         //兼容向上内联的情况
         descriptor as ParadoxCallHierarchyNodeDescriptor
         val element = descriptor.psiElement ?: return HierarchyNodeDescriptor.EMPTY_ARRAY
+        val scopeType = getScopeType()
+        val scope = ParadoxSearchScopeTypes.get(scopeType).getGlobalSearchScope(myProject, element) ?: GlobalSearchScope.allScope(myProject)
         when {
             element is ParadoxScriptScriptedVariable -> {
                 val descriptors = mutableListOf<ParadoxCallHierarchyNodeDescriptor>()
-                ReferencesSearch.search(element).processQuery {
+                ReferencesSearch.search(element, scope).processQuery {
                     val referenceElement = it.element
                     if(referenceElement.language == ParadoxScriptLanguage) {
                         val definition = referenceElement.findParentDefinition(link = true)
@@ -33,7 +38,7 @@ class ParadoxCallerHierarchyTreeStructure(
             }
             element is ParadoxScriptDefinitionElement -> {
                 val descriptors = mutableListOf<ParadoxCallHierarchyNodeDescriptor>()
-                ReferencesSearch.search(element).processQuery {
+                ReferencesSearch.search(element, scope).processQuery {
                     val referenceElement = it.element
                     if(referenceElement.language == ParadoxScriptLanguage) {
                         val definition = referenceElement.findParentDefinition(link = true)
@@ -47,5 +52,9 @@ class ParadoxCallerHierarchyTreeStructure(
             }
         }
         return HierarchyNodeDescriptor.EMPTY_ARRAY
+    }
+    
+    private fun getScopeType(): String {
+        return ParadoxHierarchyBrowserSettings.getInstance(myProject).scopeTypes.get(ParadoxHierarchyBrowserSettings.CALL) ?: "all"
     }
 }
