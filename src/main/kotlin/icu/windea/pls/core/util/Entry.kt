5526a00f74445228fa6220c4b73a0902e0d14f7d
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-04-13 09:53:07 +0800

    0.9.9 BlockEntry -> Entry

--- src/main/kotlin/icu/windea/pls/core/util/Entry.kt
+++ src/main/kotlin/icu/windea/pls/core/util/Entry.kt
@@ -1,6 +1,6 @@
 package icu.windea.pls.core.util
 
-data class BlockEntry<out K, out V>(
+data class Entry<out K, out V>(
 	val key: K,
 	val value: V
 )
--- src/main/kotlin/icu/windea/pls/tool/script/ParadoxScriptDataValueResolver.kt
+++ src/main/kotlin/icu/windea/pls/tool/script/ParadoxScriptDataValueResolver.kt
@@ -11,17 +11,17 @@ object ParadoxScriptDataValueResolver {
     /**
      * 解析脚本文件的数据。跳过不合法的[PsiElement]。
      */
-    fun resolve(file: PsiFile, conditional: Boolean = false, inline: Boolean = false): List<BlockEntry<String?, Any>> {
+    fun resolve(file: PsiFile, conditional: Boolean = false, inline: Boolean = false): List<Entry<String?, Any>> {
         if(file !is ParadoxScriptFile) throw IllegalArgumentException("Invalid file type (expect: 'ParadoxScriptFile')")
         val rootBlock = file.findChild<ParadoxScriptRootBlock>() ?: return emptyList()
         return resolveBlock(rootBlock, conditional, inline)
     }
     
-    private fun resolveBlock(block: ParadoxScriptBlockElement, conditional: Boolean = false, inline: Boolean = false): List<BlockEntry<String?, Any>> {
-        val result: MutableList<BlockEntry<String?, Any>> = SmartList()
+    private fun resolveBlock(block: ParadoxScriptBlockElement, conditional: Boolean = false, inline: Boolean = false): List<Entry<String?, Any>> {
+        val result: MutableList<Entry<String?, Any>> = SmartList()
         block.processData(conditional, inline) p@{ e ->
             when {
-                e is ParadoxScriptValue -> resolveValue(e)?.let { result.add(BlockEntry(null, it)) }
+                e is ParadoxScriptValue -> resolveValue(e)?.let { result.add(Entry(null, it)) }
                 e is ParadoxScriptProperty -> resolveProperty(e)?.let { result.add(it) }
             }
             true
@@ -42,13 +42,13 @@ object ParadoxScriptDataValueResolver {
         }
     }
     
-    fun resolveProperty(property: ParadoxScriptProperty, conditional: Boolean = false, inline: Boolean = false): BlockEntry<String?, Any>? {
+    fun resolveProperty(property: ParadoxScriptProperty, conditional: Boolean = false, inline: Boolean = false): Entry<String?, Any>? {
         val propertyValue = property.propertyValue
         if(propertyValue == null) return null //ignore
         
         //注意property的名字可以重复
         val key = property.name
         val value = resolveValue(propertyValue, conditional, inline) ?: return null
-        return BlockEntry(key, value)
+        return Entry(key, value)
     }
 }
