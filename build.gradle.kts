39824522ead807cb44640b9c5043c79361f1dcaa
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-04-25 17:22:10 +0800

    0.9.12 更新中

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "icu.windea"
-version = "0.9.11"
+version = "0.9.12"
 
 intellij {
 	pluginName.set("Paradox Language Support")
--- src/main/kotlin/icu/windea/pls/core/expression/ParadoxDefinitionSubtypeExpression.kt
+++ src/main/kotlin/icu/windea/pls/core/expression/ParadoxDefinitionSubtypeExpression.kt
@@ -6,41 +6,41 @@ import icu.windea.pls.lang.model.*
 
 /**
  * 定义子类型表达式。
- * 
+ *
  * 示例：
- * 
+ *
  * ```
  * a
  * !a
  * a&b
  * !a&!b
  * ```
- * 
+ *
  * 用途：
- * 
- * * 在CWT文件中，`subtype[X]`表示作为其值的子句中的规则仅限匹配此定义子类型表达式的定义。其中`X`即是一个定义类型表达式。 
+ *
+ * * 在CWT文件中，`subtype[X]`表示作为其值的子句中的规则仅限匹配此定义子类型表达式的定义。其中`X`即是一个定义类型表达式。
  */
 class ParadoxDefinitionSubtypeExpression(
-	expressionString: String
-): AbstractExpression(expressionString) {
-	val subtypes: List<ReversibleValue<String>>
-	
-	init {
-	    subtypes = expressionString.split('&').map { ReversibleValue(it) }
-	}
-	
-	fun matches(subtypes: Collection<String>): Boolean {
-		//目前仅支持"!"和"&"的组合
-		return this.subtypes.all { t -> t.where { subtypes.contains(it) } }
-	}
-	
-	fun matches(definitionInfo: ParadoxDefinitionInfo): Boolean {
-		return matches(definitionInfo.subtypes)
-	}
-	
-	companion object Resolver
+    expressionString: String
+) : AbstractExpression(expressionString) {
+    val subtypes: List<ReversibleValue<String>>
+    
+    init {
+        subtypes = expressionString.split('&').map { ReversibleValue(it) }
+    }
+    
+    fun matches(subtypes: Collection<String>): Boolean {
+        //目前仅支持"!"和"&"的组合
+        return this.subtypes.all { t -> t.where { subtypes.contains(it) } }
+    }
+    
+    fun matches(definitionInfo: ParadoxDefinitionInfo): Boolean {
+        return matches(definitionInfo.subtypes)
+    }
+    
+    companion object Resolver
 }
 
 fun Resolver.resolve(expressionString: String): ParadoxDefinitionSubtypeExpression {
-	return ParadoxDefinitionSubtypeExpression(expressionString)
+    return ParadoxDefinitionSubtypeExpression(expressionString)
 }
