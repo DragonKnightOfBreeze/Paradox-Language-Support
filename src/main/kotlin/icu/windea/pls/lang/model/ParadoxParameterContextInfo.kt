c9cf0f7d8a3c3fce1deab4679e56709c0de07fbf
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-05-12 22:50:28 +0800

    0.10.0 检查传入参数是否缺失时兼容参数传递的情况（不检查这种情况）

--- src/main/kotlin/icu/windea/pls/lang/model/ParadoxParameterContextInfo.kt
+++ src/main/kotlin/icu/windea/pls/lang/model/ParadoxParameterContextInfo.kt
@@ -1,8 +1,12 @@
 package icu.windea.pls.lang.model
 
 import com.intellij.psi.*
+import icu.windea.pls.config.config.*
+import icu.windea.pls.config.expression.*
 import icu.windea.pls.core.*
 import icu.windea.pls.core.psi.*
+import icu.windea.pls.lang.*
+import icu.windea.pls.script.psi.*
 import java.util.*
 
 class ParadoxParameterContextInfo(
@@ -12,11 +16,22 @@ class ParadoxParameterContextInfo(
         val parameterInfos = parameters.get(parameterName) ?: return true
         if(parameterInfos.isEmpty()) return true
         for(parameterInfo in parameterInfos) {
+            //检查参数是否带有默认值
             if(parameterInfo.defaultValue != null) return true
+            //检查参数的条件表达式上下文，基于参数名以及一组传入参数名，是否被认为是可选的
             if(parameterInfo.conditionStack.isNotNullOrEmpty()) {
                 val r = parameterInfo.conditionStack.all { rv ->
                     rv.where { parameterName == it || (argumentNames != null && argumentNames.contains(it)) }
                 }
+                if(r) return true
+            }
+        }
+        //基于参数对应的CWT规则，判断参数是否被认为是可选的
+        for(parameterInfo in parameterInfos) {
+            val configs = parameterInfo.configs
+            if(configs.isNotEmpty()) {
+                //如果作为传入参数的值，直接认为是可选的，没有太大必要进一步检查……
+                val r = configs.any { it is CwtValueConfig && it.propertyConfig?.expression?.type == CwtDataType.Parameter }
                 if(r) return true
             }
         }
@@ -34,4 +49,13 @@ class ParadoxParameterInfo(
     val conditionStack: LinkedList<ReversibleValue<String>>? = null,
 ) {
     val element: ParadoxParameter? get() = elementPointer.element
+    
+    val configs: List<CwtDataConfig<*>> by lazy {
+        val parent = element?.parent
+        when {
+            parent is ParadoxScriptPropertyKey -> ParadoxConfigHandler.getConfigs(parent)
+            parent is ParadoxScriptString -> ParadoxConfigHandler.getConfigs(parent)
+            else -> emptyList()
+        }
+    }
 }
