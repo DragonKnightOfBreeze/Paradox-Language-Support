8618bd2a6422e576568431853d9ca52fd28b3d90
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-06-08 01:26:57 +0800

    1.0.7 修复 #28 - 生成科技树时报错

--- src/main/kotlin/icu/windea/pls/core/ParadoxFileTypeOverrider.kt
+++ src/main/kotlin/icu/windea/pls/core/ParadoxFileTypeOverrider.kt
@@ -20,7 +20,7 @@ class ParadoxFileTypeOverrider : FileTypeOverrider {
     //才有可能将所在目录（以及子目录）下的文件自动识别为Paradox本地化文件和脚本文件
     
     override fun getOverriddenFileType(file: VirtualFile): FileType? {
-        //#28 - 不需要重载内存文件的文件类型
+        //不需要重载内存文件的文件类型
         if(ParadoxFileManager.isLightFile(file)) return null
         
         val fileInfo = ParadoxCoreHandler.getFileInfo(file) ?: return null
