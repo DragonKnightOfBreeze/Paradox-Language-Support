c538db48e1ca92b12472e0dafc983d009f73d465
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-06-17 17:22:32 +0800

    1.0.8 优化文件DIFF

--- src/main/kotlin/icu/windea/pls/core/diff/actions/CompareFilesAction.kt
+++ src/main/kotlin/icu/windea/pls/core/diff/actions/CompareFilesAction.kt
@@ -69,7 +69,7 @@ class CompareFilesAction : ParadoxShowDiffAction() {
         val virtualFiles = Collections.synchronizedList(mutableListOf<VirtualFile>())
         ProgressManager.getInstance().runProcessWithProgressSynchronously({
             runReadAction {
-                val selector = fileSelector(project, file).contextSensitive()
+                val selector = fileSelector(project, file)
                 val result = ParadoxFilePathSearch.search(path, null, selector, ignoreLocale = true).findAll()
                 virtualFiles.addAll(result)
             }
