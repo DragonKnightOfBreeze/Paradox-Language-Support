087e37c1922afc52be9c15b466cab064d26a8b5e
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-07-29 22:06:53 +0800

    1.1.4 优化与BUG修复

--- src/main/kotlin/icu/windea/pls/localisation/references/ParadoxLocalisationPropertyPsiReference.kt
+++ src/main/kotlin/icu/windea/pls/localisation/references/ParadoxLocalisationPropertyPsiReference.kt
@@ -5,7 +5,6 @@ package icu.windea.pls.localisation.references
 import com.intellij.openapi.util.*
 import com.intellij.psi.*
 import com.intellij.psi.impl.source.resolve.*
-import com.intellij.util.*
 import icu.windea.pls.core.*
 import icu.windea.pls.core.collections.*
 import icu.windea.pls.core.search.*
@@ -33,19 +32,13 @@ class ParadoxLocalisationPropertyPsiReference(
     
     private object Resolver : ResolveCache.AbstractResolver<ParadoxLocalisationPropertyPsiReference, PsiElement> {
         override fun resolve(ref: ParadoxLocalisationPropertyPsiReference, incompleteCode: Boolean): PsiElement? {
-            return ref.doResolve(ResolvedTypes.ALL)
+            return ref.doResolve()
         }
     }
     
     private object MultiResolver : ResolveCache.PolyVariantResolver<ParadoxLocalisationPropertyPsiReference> {
         override fun resolve(ref: ParadoxLocalisationPropertyPsiReference, incompleteCode: Boolean): Array<out ResolveResult> {
-            return ref.doMultiResolve(ResolvedTypes.ALL)
-        }
-    }
-    
-    private object LocalisationResolver : ResolveCache.AbstractResolver<ParadoxLocalisationPropertyPsiReference, ParadoxLocalisationProperty> {
-        override fun resolve(ref: ParadoxLocalisationPropertyPsiReference, incompleteCode: Boolean): ParadoxLocalisationProperty? {
-            return ref.doResolve(ResolvedTypes.Localisation).castOrNull()
+            return ref.doMultiResolve()
         }
     }
     
@@ -58,73 +51,59 @@ class ParadoxLocalisationPropertyPsiReference(
     }
     
     fun resolveLocalisation(): ParadoxLocalisationProperty? {
-        return ResolveCache.getInstance(project).resolveWithCaching(this, LocalisationResolver, false, false)
+        return ResolveCache.getInstance(project).resolveWithCaching(this, MultiResolver, false, false).castOrNull()
     }
     
-    private fun doResolve(resolvedTypes: Int): PsiElement? {
+    private fun doResolve(): PsiElement? {
         val element = element
         val file = element.containingFile as? ParadoxLocalisationFile ?: return null
         val category = ParadoxLocalisationCategory.resolve(file) ?: return null
         val locale = file.localeConfig
         val name = element.name
         
-        if(BitUtil.isSet(resolvedTypes, ResolvedTypes.Localisation)) {
-            //尝试解析成localisation或者synced_localisation
-            val selector = localisationSelector(project, file).contextSensitive().preferLocale(locale)
-            val resolved = when(category) {
-                Localisation -> ParadoxLocalisationSearch.search(name, selector).find()
-                SyncedLocalisation -> ParadoxSyncedLocalisationSearch.search(name, selector).find()
-            }
-            if(resolved != null) return resolved
+        //尝试解析成localisation或者synced_localisation
+        val selector = localisationSelector(project, file).contextSensitive().preferLocale(locale)
+        val resolved = when(category) {
+            Localisation -> ParadoxLocalisationSearch.search(name, selector).find()
+            SyncedLocalisation -> ParadoxSyncedLocalisationSearch.search(name, selector).find()
         }
+        if(resolved != null) return resolved
         
-        if(BitUtil.isSet(resolvedTypes, ResolvedTypes.Parameter)) {
-            //尝试解析成parameter
-            val resolvedParameter = ParadoxLocalisationParameterSupport.resolveParameter(element)
-            if(resolvedParameter != null) return resolvedParameter
-            
-            //尝试解析成predefined_parameter
-            val resolvedPredefinedParameter = getCwtConfig(project).core.localisationLocales.get(name)?.pointer?.element
-            if(resolvedPredefinedParameter != null) return resolvedPredefinedParameter
-        }
+        //尝试解析成parameter
+        val resolvedParameter = ParadoxLocalisationParameterSupport.resolveParameter(element)
+        if(resolvedParameter != null) return resolvedParameter
+        
+        //尝试解析成predefined_parameter
+        val resolvedPredefinedParameter = getCwtConfig(project).core.localisationLocales.get(name)?.pointer?.element
+        if(resolvedPredefinedParameter != null) return resolvedPredefinedParameter
         
         return null
     }
     
-    private fun doMultiResolve(resolvedTypes: Int): Array<out ResolveResult> {
+    private fun doMultiResolve(): Array<out ResolveResult> {
         val element = element
         val file = element.containingFile as? ParadoxLocalisationFile ?: return emptyArray()
         val category = ParadoxLocalisationCategory.resolve(file) ?: return emptyArray()
         val locale = file.localeConfig
         val name = element.name
         
-        if(BitUtil.isSet(resolvedTypes, ResolvedTypes.Localisation)) {
-            //尝试解析成localisation或者synced_localisation
-            val selector = localisationSelector(project, file).contextSensitive().preferLocale(locale)
-            val resolved = when(category) {
-                Localisation -> ParadoxLocalisationSearch.search(name, selector).findAll() //查找所有语言区域的
-                SyncedLocalisation -> ParadoxSyncedLocalisationSearch.search(name, selector).findAll() //查找所有语言区域的
-            }
-            if(resolved.isNotEmpty()) return resolved.mapToArray { PsiElementResolveResult(it) }
+        //尝试解析成localisation或者synced_localisation
+        val selector = localisationSelector(project, file).contextSensitive().preferLocale(locale)
+        val resolved = when(category) {
+            Localisation -> ParadoxLocalisationSearch.search(name, selector).findAll() //查找所有语言区域的
+            SyncedLocalisation -> ParadoxSyncedLocalisationSearch.search(name, selector).findAll() //查找所有语言区域的
         }
+        if(resolved.isNotEmpty()) return resolved.mapToArray { PsiElementResolveResult(it) }
         
-        if(BitUtil.isSet(resolvedTypes, ResolvedTypes.Parameter)) {
-            //尝试解析成parameter
-            val resolvedParameter = ParadoxLocalisationParameterSupport.resolveParameter(element)
-            if(resolvedParameter != null) return resolvedParameter.let { arrayOf(PsiElementResolveResult(it)) }
-            
-            //尝试解析成predefined_parameter
-            val resolvedPredefinedParameter = getCwtConfig(project).core.localisationLocales.get(name)?.pointer?.element
-            if(resolvedPredefinedParameter != null) return resolvedPredefinedParameter.let { arrayOf(PsiElementResolveResult(it)) }
-        }
+        //尝试解析成parameter
+        val resolvedParameter = ParadoxLocalisationParameterSupport.resolveParameter(element)
+        if(resolvedParameter != null) return resolvedParameter.let { arrayOf(PsiElementResolveResult(it)) }
+        
+        //尝试解析成predefined_parameter
+        val resolvedPredefinedParameter = getCwtConfig(project).core.localisationLocales.get(name)?.pointer?.element
+        if(resolvedPredefinedParameter != null) return resolvedPredefinedParameter.let { arrayOf(PsiElementResolveResult(it)) }
         
         return ResolveResult.EMPTY_ARRAY
-    }
-    
-    object ResolvedTypes {
-        const val Localisation = 0x01
-        const val Parameter = 0x02
-        const val ALL = Localisation or Parameter
     }
 }
 
