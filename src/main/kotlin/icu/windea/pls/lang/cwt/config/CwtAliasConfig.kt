a77b8c749b6f786d0b76e788f750acbdbb4e8a9e
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-08-26 01:25:26 +0800

    1.1.7 优化

--- src/main/kotlin/icu/windea/pls/lang/cwt/config/CwtAliasConfig.kt
+++ src/main/kotlin/icu/windea/pls/lang/cwt/config/CwtAliasConfig.kt
@@ -23,10 +23,8 @@ class CwtAliasConfig(
     val supportedScopes get() = config.supportedScopes
     val outputScope get() = config.pushScope
     
-    private val inlinedConfigCache by lazy { ContainerUtil.createConcurrentSoftKeySoftValueMap<CwtPropertyConfig, CwtPropertyConfig>() }
-    
     fun inline(config: CwtPropertyConfig): CwtPropertyConfig {
-        return inlinedConfigCache.computeIfAbsent(config) { doInline(config) }
+        return doInline(config)
     }
     
     private fun doInline(config: CwtPropertyConfig): CwtPropertyConfig {
