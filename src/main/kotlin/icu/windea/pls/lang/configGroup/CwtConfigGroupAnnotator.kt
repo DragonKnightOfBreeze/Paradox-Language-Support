aadcaba7181fce34ceb4461b49edb7bd62615e76
DragonKnightOfBreeze <dk_breeze@qq.com>
2023-11-05 22:39:18 +0800

    1.2.0 优化与BUG修复

--- src/main/kotlin/icu/windea/pls/lang/configGroup/CwtConfigGroupAnnotator.kt
+++ src/main/kotlin/icu/windea/pls/lang/configGroup/CwtConfigGroupAnnotator.kt
@@ -1,43 +1,44 @@
 package icu.windea.pls.lang.configGroup
 
 import com.intellij.ide.*
-import com.intellij.lang.annotation.*
-import com.intellij.psi.*
+import com.intellij.openapi.fileEditor.*
+import com.intellij.openapi.project.*
+import com.intellij.openapi.vfs.*
+import com.intellij.ui.*
 import icu.windea.pls.*
 import icu.windea.pls.config.configGroup.*
-import icu.windea.pls.core.*
-import icu.windea.pls.cwt.psi.*
+import icu.windea.pls.cwt.*
+import java.util.function.Function
+import javax.swing.*
 
 /**
  * 当用户打开一个可用于自定义CWT规则分组的CWT文件时，给出提示以及一些参考信息。
  */
-class CwtConfigGroupAnnotator : Annotator {
-    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        when(element) {
-            is CwtFile -> annotateFile(element, holder)
-        }
-    }
-    
-    private fun annotateFile(file: CwtFile, holder: AnnotationHolder) {
-        val project = file.project
-        val vFile = file.virtualFile ?: return
+class CwtConfigGroupEditorNotificationProvider : EditorNotificationProvider{
+    override fun collectNotificationData(project: Project, file: VirtualFile): Function<in FileEditor, out JComponent?>? {
+        if(file.fileType != CwtFileType) return null
+        
         val fileProviders = CwtConfigGroupFileProvider.EP_NAME.extensionList
         val configGroups = mutableSetOf<CwtConfigGroup>()
         fileProviders.forEach { fileProvider ->
-            configGroups += fileProvider.getConfigGroups(project, vFile)
+            configGroups += fileProvider.getConfigGroups(project, file)
         }
-        if(configGroups.isEmpty()) return
-        val message = PlsBundle.message("configGroup.annotator.file.message")
-        holder.newAnnotation(HighlightSeverity.INFORMATION, message).fileLevel()
-            .withFix(createIntentionAction(PlsBundle.message("configGroup.annotator.file.fix.guidance")) { _, _, _ ->
+        if(configGroups.isEmpty()) return null
+        
+        return Function { fileEditor ->
+            val panel = EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Info)
+                .text(PlsBundle.message("configGroup.config.file.message"))
+            panel.createActionLabel(PlsBundle.message("configGroup.config.file.guidance")) {
                 val url = "https://github.com/DragonKnightOfBreeze/Paradox-Language-Support/blob/master/references/cwt/guidance.md"
                 //val url = "https://github.com/cwtools/cwtools/wiki/.cwt-config-file-guidance"
                 BrowserUtil.browse(url)
-            })
-            .withFix(createIntentionAction(PlsBundle.message("configGroup.annotator.file.fix.repositories")) { _, _, _ ->
+            }
+            panel.createActionLabel(PlsBundle.message("configGroup.config.file.repositories")) {
                 val url = "https://github.com/DragonKnightOfBreeze/Paradox-Language-Support/tree/master/src/main/resources/config"
                 BrowserUtil.browse(url)
-            })
-            .create()
+            }
+            panel
+        }
     }
 }
+
