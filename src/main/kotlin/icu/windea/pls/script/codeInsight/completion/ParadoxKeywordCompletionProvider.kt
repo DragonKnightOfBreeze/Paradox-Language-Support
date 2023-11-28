c018f21022060d47a4ef030549d2f0b104f58696
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-11-28 11:56:00 +0800

    1.2.6 优化为脚本文件提供的关键字的代码补全

--- src/main/kotlin/icu/windea/pls/script/codeInsight/completion/ParadoxKeywordCompletionProvider.kt
+++ src/main/kotlin/icu/windea/pls/script/codeInsight/completion/ParadoxKeywordCompletionProvider.kt
@@ -2,12 +2,14 @@ package icu.windea.pls.script.codeInsight.completion
 
 import com.intellij.codeInsight.completion.*
 import com.intellij.util.*
+import icu.windea.pls.config.config.*
 import icu.windea.pls.core.*
 import icu.windea.pls.core.codeInsight.completion.*
+import icu.windea.pls.lang.*
 import icu.windea.pls.script.psi.*
 
 /**
- * 提供关键字（布尔值或子句）的代码补全（不在定义声明中提供）。
+ * 提供关键字的代码补全（要求不在定义声明中提供）。
  */
 class ParadoxKeywordCompletionProvider : CompletionProvider<CompletionParameters>() {
 	override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
@@ -16,7 +18,9 @@ class ParadoxKeywordCompletionProvider : CompletionProvider<CompletionParameters
 		if(element.text.isLeftQuoted()) return
 		if(element.text.isParameterized()) return
 		if(element.isExpression()) {
-			if(element.findParentDefinition() != null) return
+			//判断光标位置是否在定义声明中，更加准确，更具兼容性
+			val configContext = CwtConfigHandler.getConfigContext(element)
+			if(configContext != null && configContext.isRootOrMember()) return
 		}
 		
 		result.addElement(PlsLookupElements.yesLookupElement)
