b69cba1855a7d76e85717cb3178a817764b2fcc2
DragonKnigtOfBreeze <dk_breeze@qq.com>
2023-12-26 20:12:01 +0800

    1.3.0 兼容整行或多行参数值中对双引号的特别转义 - 更新中

--- src/main/kotlin/icu/windea/pls/script/psi/ParadoxScriptStringExpressionElement.kt
+++ src/main/kotlin/icu/windea/pls/script/psi/ParadoxScriptStringExpressionElement.kt
@@ -2,8 +2,9 @@ package icu.windea.pls.script.psi
 
 import com.intellij.openapi.util.*
 import com.intellij.psi.*
+import com.intellij.psi.impl.source.tree.java.*
 import icu.windea.pls.core.psi.*
-import icu.windea.pls.util.*
+import kotlin.math.*
 
 /**
  * @see ParadoxScriptPropertyKey
@@ -26,14 +27,19 @@ interface ParadoxScriptStringExpressionElement : ParadoxScriptExpressionElement,
     
     override fun createLiteralTextEscaper(): LiteralTextEscaper<ParadoxScriptStringExpressionElement> {
         return object : LiteralTextEscaper<ParadoxScriptStringExpressionElement>(this) {
+            private var outSourceOffsets: IntArray? = null
+            
             override fun decode(rangeInsideHost: TextRange, outChars: StringBuilder): Boolean {
-                val s = rangeInsideHost.substring(myHost.text)
-                ParadoxEscapeManager.escapeScriptExpression(s, outChars)
-                return true
+                val subText = rangeInsideHost.substring(myHost.text)
+                outSourceOffsets = IntArray(subText.length + 1)
+                return PsiLiteralExpressionImpl.parseStringCharacters(subText, outChars, outSourceOffsets)
             }
             
             override fun getOffsetInHost(offsetInDecoded: Int, rangeInsideHost: TextRange): Int {
-                return rangeInsideHost.startOffset + offsetInDecoded
+                val outSourceOffsets = outSourceOffsets!!
+                val result = if(offsetInDecoded < outSourceOffsets.size) outSourceOffsets[offsetInDecoded] else -1
+                if(result == -1) return -1
+                return min(result, rangeInsideHost.length) + rangeInsideHost.startOffset
             }
             
             override fun isOneLine(): Boolean {
