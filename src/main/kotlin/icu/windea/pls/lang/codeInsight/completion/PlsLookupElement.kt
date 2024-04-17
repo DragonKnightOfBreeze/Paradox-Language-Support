cb129e47877f6137997d67f99302801c0d2c0e1c
DragonKnigtOfBreeze <dk_breeze@qq.com>
2024-04-17 17:18:16 +0800

    1.3.5 重构代码补全相关代码

--- src/main/kotlin/icu/windea/pls/lang/codeInsight/completion/PlsLookupElement.kt
+++ src/main/kotlin/icu/windea/pls/lang/codeInsight/completion/PlsLookupElement.kt
@@ -3,6 +3,6 @@ package icu.windea.pls.lang.codeInsight.completion
 import com.intellij.codeInsight.lookup.*
 
 class PlsLookupElement(
-    val delegate: LookupElement,
+    delegate: LookupElement,
     val extraElements: List<LookupElement> = emptyList()
 ) : LookupElementDecorator<LookupElement>(delegate)
