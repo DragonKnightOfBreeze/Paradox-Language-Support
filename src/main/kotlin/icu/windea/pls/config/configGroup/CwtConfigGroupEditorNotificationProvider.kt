ef86a1105cfc5342e60c8937bbecfd2301d20fe5
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-04-28 01:15:50 +0800

    1.3.6 在内置规则文件的编辑器通知中提供操作，点击可以生成或者导航到对应的项目本地的规则文件

--- src/main/kotlin/icu/windea/pls/config/configGroup/CwtConfigGroupEditorNotificationProvider.kt
+++ src/main/kotlin/icu/windea/pls/config/configGroup/CwtConfigGroupEditorNotificationProvider.kt
@@ -1,11 +1,13 @@
 package icu.windea.pls.config.configGroup
 
 import com.intellij.ide.*
+import com.intellij.openapi.application.*
 import com.intellij.openapi.fileEditor.*
 import com.intellij.openapi.project.*
 import com.intellij.openapi.vfs.*
 import com.intellij.ui.*
 import icu.windea.pls.*
+import icu.windea.pls.core.*
 import icu.windea.pls.cwt.*
 import icu.windea.pls.ep.configGroup.*
 import java.util.function.Function
@@ -23,11 +25,32 @@ class CwtConfigGroupEditorNotificationProvider : EditorNotificationProvider, Dum
         if(fileProvider == null) return null
         
         return Function { fileEditor ->
+            val builtIn = fileProvider.isBuiltIn()
             val message = when {
-                fileProvider.isBuiltIn() -> PlsBundle.message("configGroup.config.file.message.1")
+                builtIn -> PlsBundle.message("configGroup.config.file.message.1")
                 else -> PlsBundle.message("configGroup.config.file.message.2")
             }
             val panel = EditorNotificationPanel(fileEditor, EditorNotificationPanel.Status.Info).text(message)
+            run {
+                if(!builtIn) return@run
+                val rootDirectory = fileProvider.getRootDirectory(project) ?: return@run
+                val relPath = VfsUtil.getRelativePath(file, rootDirectory) ?: return@run
+                val projectLocalRootDirectory = fileProviders.find { it is ProjectCwtConfigGroupFileProvider }
+                    ?.getRootDirectory(project) ?: return@run
+                val destFile = VfsUtil.findRelativeFile(relPath, projectLocalRootDirectory)
+                panel.createActionLabel(PlsBundle.message("configGroup.config.file.customize")) action@{
+                    if(destFile != null && destFile.exists()) {
+                        destFile.toPsiFile(project)?.navigate(true)
+                    } else {
+                        runWriteAction {
+                            val newFile = projectLocalRootDirectory.findOrCreateFile(relPath)
+                            newFile.writeText(file.readText())
+                            newFile.toPsiFile(project)?.navigate(true)
+                        }
+                    }
+                }
+                
+            }
             panel.createActionLabel(PlsBundle.message("configGroup.config.file.guidance")) {
                 val url = "https://github.com/DragonKnightOfBreeze/Paradox-Language-Support/blob/master/references/cwt/guidance.md"
                 //val url = "https://github.com/cwtools/cwtools/wiki/.cwt-config-file-guidance"
