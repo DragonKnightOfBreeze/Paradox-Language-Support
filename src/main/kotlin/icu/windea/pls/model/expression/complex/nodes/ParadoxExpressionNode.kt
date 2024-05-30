5ceaff63a9f96344d50e9392dfbb76a998a36e38
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-05-30 22:59:51 +0800

    1.3.9 优化与重构复杂表达式相关代码

--- src/main/kotlin/icu/windea/pls/model/expression/complex/nodes/ParadoxExpressionNode.kt
+++ src/main/kotlin/icu/windea/pls/model/expression/complex/nodes/ParadoxExpressionNode.kt
@@ -7,6 +7,11 @@ import icu.windea.pls.model.*
 import icu.windea.pls.model.expression.complex.errors.*
 import icu.windea.pls.script.psi.*
 
+/**
+ * 复杂表达式的节点。复杂表达式由数个节点组成，本身也是一个节点。
+ * 
+ * 对应对应范围的文本，可以指定如何进行代码高亮，以及如何解析为引用，以及解析失败时的错误提示。
+ */
 interface ParadoxExpressionNode : AttributesKeyAware {
     val text: String
     val rangeInExpression: TextRange
