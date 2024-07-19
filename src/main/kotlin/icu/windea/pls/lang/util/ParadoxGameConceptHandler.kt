ed1abf7475effccf5aac20ce127845cfc0ec4b04
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-07-19 16:05:11 +0800

    1.3.15 正确渲染从数据库对象生成的概念的文本

--- src/main/kotlin/icu/windea/pls/lang/util/ParadoxGameConceptHandler.kt
+++ src/main/kotlin/icu/windea/pls/lang/util/ParadoxGameConceptHandler.kt
@@ -2,10 +2,12 @@ package icu.windea.pls.lang.util
 
 import com.intellij.openapi.project.*
 import com.intellij.psi.*
+import icu.windea.pls.core.*
 import icu.windea.pls.ep.data.*
 import icu.windea.pls.lang.search.*
 import icu.windea.pls.lang.search.selector.*
 import icu.windea.pls.localisation.psi.*
+import icu.windea.pls.model.expression.complex.nodes.*
 import icu.windea.pls.script.psi.*
 
 object ParadoxGameConceptHandler {
@@ -16,10 +18,22 @@ object ParadoxGameConceptHandler {
         return ParadoxDefinitionSearch.search("game_concept", definitionSelector).find()
     }
     
+    /**
+     * @return [ParadoxLocalisationConceptText] or [ParadoxLocalisationProperty]
+     */
     fun getTextElement(element: ParadoxLocalisationConcept): PsiElement? {
         val conceptText = element.conceptText
         if(conceptText != null) return conceptText
-        val resolved = element.reference?.resolve() ?: return null
-        return ParadoxDefinitionHandler.getPrimaryLocalisation(resolved)
+        run {
+            val resolved = element.reference?.resolve() ?: return@run
+            return ParadoxDefinitionHandler.getPrimaryLocalisation(resolved)
+        }
+        run {
+            val resolved = element.conceptName?.references?.findLast { it is ParadoxDatabaseObjectNode.Reference }
+                ?.resolve()?.castOrNull<ParadoxScriptDefinitionElement>()
+                ?: return@run
+            return ParadoxDefinitionHandler.getPrimaryLocalisation(resolved)
+        }
+        return null
     }
 }
