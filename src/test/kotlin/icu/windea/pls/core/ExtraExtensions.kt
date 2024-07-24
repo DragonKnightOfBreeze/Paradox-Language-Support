38487c07c69cdddd05a5dd150235890e7ab4f2e1
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-07-24 12:08:26 +0800

    1.3.15 更新

---
+++ src/test/kotlin/icu/windea/pls/core/ExtraExtensions.kt
@@ -0,0 +1,26 @@
+package icu.windea.pls.core
+
+import kotlin.reflect.*
+
+inline fun <T> withProperties(block: PropertiesContainer.() -> T): T {
+    val container = PropertiesContainer()
+    try {
+        return container.block()
+    } finally {
+        for((property, oldValue) in container.properties2OldValues) {
+            @Suppress("UNCHECKED_CAST")
+            (property as KMutableProperty0<Any?>).set(oldValue)
+        }
+    }
+}
+
+class PropertiesContainer {
+    val properties2OldValues : MutableMap<KMutableProperty0<*>, Any?> = mutableMapOf()
+    
+    @Suppress("NOTHING_TO_INLINE")
+    inline fun <T> KMutableProperty0<T>.register(): T {
+        val oldValue = this.get()
+        properties2OldValues[this] = oldValue
+        return oldValue
+    }
+}
