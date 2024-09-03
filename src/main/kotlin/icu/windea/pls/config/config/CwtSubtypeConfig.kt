e20e249b388647528b70bbdb5e7930c672395688
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-09-03 16:50:05 +0800

    1.3.20 优化

--- src/main/kotlin/icu/windea/pls/config/config/CwtSubtypeConfig.kt
+++ src/main/kotlin/icu/windea/pls/config/config/CwtSubtypeConfig.kt
@@ -44,30 +44,28 @@ private fun doResolve(config: CwtPropertyConfig): CwtSubtypeConfig? {
     var abbreviation: String? = null
     var onlyIfNot: Set<String>? = null
     
-    val options = config.optionConfigs
-    if(!options.isNullOrEmpty()) {
-        for(option in options) {
-            if(option !is CwtOptionConfig) continue
-            val key = option.key
-            when(key) {
-                "type_key_filter" -> {
-                    //值可能是string也可能是stringArray
-                    val values = option.getOptionValueOrValues()
-                    if(values == null) continue
-                    val set = caseInsensitiveStringSet() //忽略大小写
-                    set.addAll(values)
-                    val o = option.separatorType == CwtSeparatorType.EQUAL
-                    typeKeyFilter = set reverseIf o
-                }
-                "type_key_regex" -> {
-                    typeKeyRegex = option.stringValue?.toRegex(RegexOption.IGNORE_CASE)
-                }
-                "starts_with" -> startsWith = option.stringValue ?: continue //忽略大小写
-                "push_scope" -> pushScope = option.stringValue ?: continue
-                "display_name" -> displayName = option.stringValue ?: continue
-                "abbreviation" -> abbreviation = option.stringValue ?: continue
-                "only_if_not" -> onlyIfNot = option.getOptionValueOrValues() ?: continue
+    val options = config.optionConfigs.orEmpty()
+    for(option in options) {
+        if(option !is CwtOptionConfig) continue
+        val key = option.key
+        when(key) {
+            "type_key_filter" -> {
+                //值可能是string也可能是stringArray
+                val values = option.getOptionValueOrValues()
+                if(values == null) continue
+                val set = caseInsensitiveStringSet() //忽略大小写
+                set.addAll(values)
+                val o = option.separatorType == CwtSeparatorType.EQUAL
+                typeKeyFilter = set reverseIf o
             }
+            "type_key_regex" -> {
+                typeKeyRegex = option.stringValue?.toRegex(RegexOption.IGNORE_CASE)
+            }
+            "starts_with" -> startsWith = option.stringValue ?: continue //忽略大小写
+            "push_scope" -> pushScope = option.stringValue ?: continue
+            "display_name" -> displayName = option.stringValue ?: continue
+            "abbreviation" -> abbreviation = option.stringValue ?: continue
+            "only_if_not" -> onlyIfNot = option.getOptionValueOrValues() ?: continue
         }
     }
     return CwtSubtypeConfigImpl(config, name, typeKeyFilter, typeKeyRegex, startsWith, pushScope, displayName, abbreviation, onlyIfNot?.optimized())
