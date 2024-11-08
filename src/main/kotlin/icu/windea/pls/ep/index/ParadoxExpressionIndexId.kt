169876ad4d2d101a2f127b968805ef9a6afb3d12
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-11-08 18:17:40 +0800

    1.3.24 优化

--- src/main/kotlin/icu/windea/pls/ep/index/ParadoxExpressionIndexId.kt
+++ src/main/kotlin/icu/windea/pls/ep/index/ParadoxExpressionIndexId.kt
@@ -3,14 +3,14 @@ package icu.windea.pls.ep.index
 import icu.windea.pls.model.expressionInfo.*
 
 sealed class ParadoxExpressionIndexId<T : ParadoxExpressionInfo>(val code: Byte) {
-    object ComplexEnumValue : ParadoxExpressionIndexId<ParadoxComplexEnumValueInfo>(1)
-    object DynamicValue : ParadoxExpressionIndexId<ParadoxDynamicValueInfo>(2)
-    object InlineScriptUsage : ParadoxExpressionIndexId<ParadoxInlineScriptUsageInfo>(3)
-    object Parameter : ParadoxExpressionIndexId<ParadoxParameterInfo>(4)
-    object LocalisationParameter : ParadoxExpressionIndexId<ParadoxLocalisationParameterInfo>(5)
+    data object ComplexEnumValue : ParadoxExpressionIndexId<ParadoxComplexEnumValueInfo>(1)
+    data object DynamicValue : ParadoxExpressionIndexId<ParadoxDynamicValueInfo>(2)
+    data object InlineScriptUsage : ParadoxExpressionIndexId<ParadoxInlineScriptUsageInfo>(3)
+    data object Parameter : ParadoxExpressionIndexId<ParadoxParameterInfo>(4)
+    data object LocalisationParameter : ParadoxExpressionIndexId<ParadoxLocalisationParameterInfo>(5)
 
-    object InferredScopeContextAwareDefinition : ParadoxExpressionIndexId<ParadoxInferredScopeContextAwareDefinitionInfo>(11)
-    object EventInOnAction : ParadoxExpressionIndexId<ParadoxEventInOnActionInfo>(12)
-    object EventInEvent : ParadoxExpressionIndexId<ParadoxEventInEventInfo>(13)
-    object OnActionInEvent : ParadoxExpressionIndexId<ParadoxOnActionInEventInfo>(14)
+    data object InferredScopeContextAwareDefinition : ParadoxExpressionIndexId<ParadoxInferredScopeContextAwareDefinitionInfo>(11)
+    data object EventInOnAction : ParadoxExpressionIndexId<ParadoxEventInOnActionInfo>(12)
+    data object EventInEvent : ParadoxExpressionIndexId<ParadoxEventInEventInfo>(13)
+    data object OnActionInEvent : ParadoxExpressionIndexId<ParadoxOnActionInEventInfo>(14)
 }
