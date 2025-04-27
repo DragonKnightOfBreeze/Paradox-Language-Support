c8ca9d9377d70954149b0ee7ad125678de2159cf
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-04-27 18:13:03 +0800

    #137 准备工作

--- src/main/kotlin/icu/windea/pls/lang/ParadoxBaseLanguage.kt
+++ src/main/kotlin/icu/windea/pls/lang/ParadoxBaseLanguage.kt
@@ -2,7 +2,7 @@ package icu.windea.pls.lang
 
 import com.intellij.lang.*
 
-open class ParadoxBaseLanguage(ID:String): Language(ID) {
+open class ParadoxBaseLanguage(ID: String) : Language(ID), DependentLanguage {
     companion object {
         @JvmField
         val INSTANCE = ParadoxBaseLanguage("Paradox")
