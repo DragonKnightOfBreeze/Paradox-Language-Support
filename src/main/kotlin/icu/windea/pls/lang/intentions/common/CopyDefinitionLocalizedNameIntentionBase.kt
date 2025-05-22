27633c593f8427f057f6229ef4b914546beade70
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-05-23 01:07:18 +0800

    优化意向操作，改写为 ModCommandAction

--- src/main/kotlin/icu/windea/pls/lang/intentions/common/CopyDefinitionLocalizedNameIntentionBase.kt
+++ src/main/kotlin/icu/windea/pls/lang/intentions/common/CopyDefinitionLocalizedNameIntentionBase.kt
@@ -1,50 +1,35 @@
 package icu.windea.pls.lang.intentions.common
 
-import com.intellij.codeInsight.intention.*
-import com.intellij.codeInsight.intention.preview.*
-import com.intellij.openapi.editor.*
-import com.intellij.openapi.ide.*
-import com.intellij.openapi.project.*
-import com.intellij.psi.*
+import com.intellij.modcommand.*
 import icu.windea.pls.*
 import icu.windea.pls.lang.util.*
 import icu.windea.pls.script.psi.*
-import java.awt.datatransfer.*
 
 /**
  * 复制定义的本地化名字到剪贴板。
  */
-abstract class CopyDefinitionLocalizedNameIntentionBase : IntentionAction {
-    override fun getText() = PlsBundle.message("intention.copyDefinitionLocalizedName")
+abstract class CopyDefinitionLocalizedNameIntentionBase : ModCommandAction {
+    override fun getFamilyName() = PlsBundle.message("intention.copyDefinitionLocalizedName")
 
-    override fun getFamilyName() = text
-
-    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
-        if (editor == null || file == null) return false
-        val offset = editor.caretModel.offset
-        return getLocalizedName(file, offset) != null
+    override fun getPresentation(context: ActionContext): Presentation? {
+        val text = getText(context) ?: return null
+        return Presentation.of(familyName)
     }
 
-    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-        if (editor == null || file == null) return
-        val offset = editor.caretModel.offset
-        val text = getLocalizedName(file, offset) ?: return
-        CopyPasteManager.getInstance().setContents(StringSelection(text))
+    override fun perform(context: ActionContext): ModCommand {
+        val text = getText(context) ?: return ModCommand.nop()
+        return ModCommand.copyToClipboard(text)
     }
 
-    private fun findElement(file: PsiFile, offset: Int): ParadoxScriptDefinitionElement? {
-        val allOptions = ParadoxPsiManager.FindDefinitionOptions
-        val options = allOptions.DEFAULT or allOptions.BY_REFERENCE
-        return ParadoxPsiManager.findDefinition(file, offset, options)
-    }
-
-    private fun getLocalizedName(file: PsiFile, offset: Int): String? {
-        val element = findElement(file, offset) ?: return null
+    private fun getText(context: ActionContext): String? {
+        val element = findElement(context) ?: return null
         return ParadoxDefinitionManager.getLocalizedNames(element).firstOrNull()
     }
 
-    override fun generatePreview(project: Project, editor: Editor, file: PsiFile) = IntentionPreviewInfo.EMPTY
-
-    override fun startInWriteAction() = false
+    private fun findElement(context: ActionContext): ParadoxScriptDefinitionElement? {
+        val allOptions = ParadoxPsiManager.FindDefinitionOptions
+        val options = allOptions.DEFAULT or allOptions.BY_REFERENCE
+        return ParadoxPsiManager.findDefinition(context.file, context.offset, options)
+    }
 }
 
--- src/main/kotlin/icu/windea/pls/lang/intentions/common/CopyLocalisationNameIntentionBase.kt
+++ src/main/kotlin/icu/windea/pls/lang/intentions/common/CopyLocalisationNameIntentionBase.kt
@@ -1,51 +1,36 @@
 package icu.windea.pls.lang.intentions.common
 
-import com.intellij.codeInsight.intention.*
-import com.intellij.codeInsight.intention.preview.*
-import com.intellij.openapi.editor.*
-import com.intellij.openapi.ide.*
-import com.intellij.openapi.project.*
-import com.intellij.psi.*
+import com.intellij.modcommand.*
 import icu.windea.pls.*
 import icu.windea.pls.core.*
 import icu.windea.pls.lang.util.*
 import icu.windea.pls.localisation.psi.*
-import java.awt.datatransfer.*
 
 /**
  * 复制本地化的名字到剪贴板。
  */
-abstract class CopyLocalisationNameIntentionBase : IntentionAction {
-    override fun getText() = PlsBundle.message("intention.copyLocalisationName")
+abstract class CopyLocalisationNameIntentionBase : ModCommandAction {
+    override fun getFamilyName() = PlsBundle.message("intention.copyLocalisationName")
 
-    override fun getFamilyName() = text
-
-    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
-        if (editor == null || file == null) return false
-        val offset = editor.caretModel.offset
-        return getName(file, offset) != null
+    override fun getPresentation(context: ActionContext): Presentation? {
+        val text = getText(context) ?: return null
+        return Presentation.of(familyName)
     }
 
-    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-        if (editor == null || file == null) return
-        val offset = editor.caretModel.offset
-        val text = getName(file, offset) ?: return
-        CopyPasteManager.getInstance().setContents(StringSelection(text))
+    override fun perform(context: ActionContext): ModCommand {
+        val text = getText(context) ?: return ModCommand.nop()
+        return ModCommand.copyToClipboard(text)
     }
 
-    private fun findElement(file: PsiFile, offset: Int): ParadoxLocalisationProperty? {
-        val allOptions = ParadoxPsiManager.FindLocalisationOptions
-        val options = allOptions.DEFAULT or allOptions.BY_REFERENCE
-        return ParadoxPsiManager.findLocalisation(file, offset, options)
-    }
-
-    private fun getName(file: PsiFile, offset: Int): String? {
-        val element = findElement(file, offset) ?: return null
+    private fun getText(context: ActionContext): String? {
+        val element = findElement(context) ?: return null
         return element.name.orNull()
     }
 
-    override fun generatePreview(project: Project, editor: Editor, file: PsiFile) = IntentionPreviewInfo.EMPTY
-
-    override fun startInWriteAction() = false
+    private fun findElement(context: ActionContext): ParadoxLocalisationProperty? {
+        val allOptions = ParadoxPsiManager.FindLocalisationOptions
+        val options = allOptions.DEFAULT or allOptions.BY_REFERENCE
+        return ParadoxPsiManager.findLocalisation(context.file, context.offset, options)
+    }
 }
 
