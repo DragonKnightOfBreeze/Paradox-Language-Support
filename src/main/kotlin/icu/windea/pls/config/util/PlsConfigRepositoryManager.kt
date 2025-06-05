3cd996b65371e601d7921979b345e580c51a01f1
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-06-05 17:39:46 +0800

    PlsConfigRepositoryManager 提取本地化文本

--- src/main/kotlin/icu/windea/pls/config/util/PlsConfigRepositoryManager.kt
+++ src/main/kotlin/icu/windea/pls/config/util/PlsConfigRepositoryManager.kt
@@ -36,19 +36,19 @@ object PlsConfigRepositoryManager {
     fun validateUrl(builder: ValidationInfoBuilder, gameType: ParadoxGameType, url: String): ValidationInfo? {
         //规则仓库URL应当包含对应的游戏类型ID
 
-        if (url.isEmpty()) return builder.warning("The repository url is not configured")
-        if (!url.contains(gameType.id)) return builder.error("The repository url must contain game type id '${gameType.id}'")
+        if (url.isEmpty()) return builder.warning(PlsBundle.message("config.repo.validation.urlNotConfigured"))
+        if (!url.contains(gameType.id)) return builder.error(PlsBundle.message("config.repo.validation.urlMustContainGameTypeId", gameType.id))
         return null
     }
 
     @Synchronized
     fun validateUrlsByGit(urls: List<String>): Boolean {
-        //NOTE 这里的需要执行git命令的校验，需要并发进行
+        //NOTE 这里需要执行git命令的校验，需要并发进行
 
-        val results = runWithModalProgressBlocking(ModalTaskOwner.guess(), "Validation Config Repository Urls") r@{
+        val results = runWithModalProgressBlocking(ModalTaskOwner.guess(), PlsBundle.message("config.repo.validation.progress.title")) r@{
             reportRawProgress { reporter ->
-                reporter.text("Validate config repository urls...")
-                reporter.details("Execute 'git ls-remote' commands asynchronously...")
+                reporter.text(PlsBundle.message("config.repo.validation.progress.text"))
+                reporter.details(PlsBundle.message("config.repo.validation.progress.details"))
 
                 val resultFutures = urls.map {
                     async {
@@ -67,12 +67,12 @@ object PlsConfigRepositoryManager {
                 .mapNotNull { it.exceptionOrNull()?.message?.orNull() }
                 .distinct()
                 .joinToString("<br>") { it.replace("\n", "<br>") }
-            Messages.showErrorDialog(errorMessage, "Validation Result")
+            Messages.showErrorDialog(errorMessage, PlsBundle.message("config.repo.validation.result.title"))
             return false
         }
 
         //显示成功的消息弹窗
-        Messages.showInfoMessage("Success. All config repository urls are connectable.", "Validation Result")
+        Messages.showInfoMessage(PlsBundle.message("config.repo.validation.result.0"), PlsBundle.message("config.repo.validation.result.title"))
         return true
     }
 
@@ -106,16 +106,16 @@ object PlsConfigRepositoryManager {
         //创建父目录，如果存在报错，发送通知并直接返回
         val r = runCatchingCancelable { parentDirectory.toPath().createDirectories() }
         if (r.isFailure) {
-            val warningMessage = "Failed. Cannot create remote config directory."
-            createNotification("Synchronize Result", warningMessage, NotificationType.ERROR).notify(project)
+            val warningMessage = PlsBundle.message("config.repo.sync.createDirectoryFailed")
+            createNotification(PlsBundle.message("config.repo.sync.result.title"), warningMessage, NotificationType.ERROR).notify(project)
             return
         }
 
         PlsFacade.getCoroutineScope().launch c@{
-            val results = withBackgroundProgress(project, "Synchronize from Config Repository Urls", cancellable = true) p@{
+            val results = withBackgroundProgress(project, PlsBundle.message("config.repo.sync.progress.title"), cancellable = true) p@{
                 reportRawProgress { reporter ->
-                    reporter.text("Synchronize from config repository urls...")
-                    reporter.details("Execute 'git clone' or 'git pull' commands asynchronously...")
+                    reporter.text(PlsBundle.message("config.repo.sync.progress.text"))
+                    reporter.details(PlsBundle.message("config.repo.sync.progress.details"))
 
                     val resultFutures = mutableListOf<Deferred<Result<String>>>()
                     urlMap.forEach { (_, url) ->
@@ -129,14 +129,14 @@ object PlsConfigRepositoryManager {
                 }
             }
 
-            val action = NotificationAction.createSimple("Open remote config directory") {
+            val action = NotificationAction.createSimple(PlsBundle.message("config.repo.sync.action.openRepoDir")) {
                 BrowserUtil.open(parentDirectory)
             }
 
             //如果存在报错，发送通知并直接返回
             if (results.any { it.isFailure }) {
-                val warningMessage = "Failed. Please check it manually."
-                val notification = createNotification("Synchronize Result", warningMessage, NotificationType.WARNING)
+                val warningMessage = PlsBundle.message("config.repo.sync.result.2")
+                val notification = createNotification(PlsBundle.message("config.repo.sync.result.title"), warningMessage, NotificationType.WARNING)
                     .addAction(action)
                 openProjects.forEach { notification.notify(it) }
                 return@c
@@ -145,9 +145,9 @@ object PlsConfigRepositoryManager {
             val updated = results.any { result -> result.getOrNull().let { !PlsGitManager.isUpdateToDate(it) } }
 
             //发送成功的通知
-            val successMessage = if (updated) "Success. All config repositories are updated if necessary."
-            else "Success. All config repositories are up to date."
-            val notification = createNotification("Synchronize Result", successMessage, NotificationType.INFORMATION)
+            val successMessage = if (updated) PlsBundle.message("config.repo.sync.result.0")
+            else PlsBundle.message("config.repo.sync.result.1")
+            val notification = createNotification(PlsBundle.message("config.repo.sync.result.title"), successMessage, NotificationType.INFORMATION)
                 .addAction(action)
             openProjects.forEach { notification.notify(it) }
 
@@ -156,8 +156,6 @@ object PlsConfigRepositoryManager {
                 val messageBus = ApplicationManager.getApplication().messageBus
                 messageBus.syncPublisher(ParadoxConfigDirectoriesListener.TOPIC).onChange()
             }
-
-            //TODO 1.4.2 适用刷新规则分组数据后，之后的功能全部正常
         }
     }
 
