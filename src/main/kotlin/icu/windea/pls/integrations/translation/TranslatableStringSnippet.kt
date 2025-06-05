3d4fe2618ee108007a89b15b62804cf02239e4cf
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-06-05 22:48:17 +0800

    修复兼容性问题

--- src/main/kotlin/icu/windea/pls/integrations/translation/TranslatableStringSnippet.kt
+++
@@ -1,9 +0,0 @@
-package icu.windea.pls.integrations.translation
-
-import cn.yiiguxing.plugin.translate.trans.*
-
-data class TranslatableStringSnippet(
-    var text: String,
-    val shouldTranslate: Boolean,
-    val lang: Lang,
-)
