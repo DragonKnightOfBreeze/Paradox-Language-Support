ef80bfdd1f10557cb791010663e3355bf2bb5cf1
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-10-02 15:47:14 +0800

    完善语法测试

--- src/test/kotlin/icu/windea/pls/localisation/ParadoxLocalisationMoreSyntaxPsiTest.kt
+++ src/test/kotlin/icu/windea/pls/localisation/ParadoxLocalisationMoreSyntaxPsiTest.kt
@@ -17,4 +17,18 @@ class ParadoxLocalisationMoreSyntaxPsiTest : ParsingTestCase("localisation/synta
     fun test_text_formats_ck3() = doTest(true)
     fun test_text_formats_stellaris() = doTest(true)
     fun test_text_icons_vic3() = doTest(true)
+
+    // Added tests for header detection and trailing comment after value
+    fun test_only_header_eof() = doTest(true)
+    fun test_header_as_key() = doTest(true)
+    fun test_value_trailing_comment() = doTest(true)
+
+    // Debug: dump actual parse tree without assertion
+    fun test_value_trailing_comment_dump_only() {
+        val name = "_value_trailing_comment"
+        val file = createPsiFile(name, loadFile("$name.test.yml"))
+        ensureParsed(file)
+        val actual = toParseTreeText(file, skipSpaces(), includeRanges())
+        println(actual)
+    }
 }
