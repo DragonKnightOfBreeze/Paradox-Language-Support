33c13bb7cfd2ed58cb0fb25fe634f6b0639baf03
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-11-08 04:17:58 +0800

    最终发布前再次更新索引版本

--- src/main/kotlin/icu/windea/pls/lang/index/PlsIndexVersions.kt
+++ src/main/kotlin/icu/windea/pls/lang/index/PlsIndexVersions.kt
@@ -1,12 +1,12 @@
 package icu.windea.pls.lang.index
 
 object PlsIndexVersions {
-    const val ScriptStub = 77 // VERSION for 2.0.6
-    const val LocalisationStub = 77 // VERSION for 2.0.6
+    const val ScriptStub = 78 // VERSION for 2.0.6 released
+    const val LocalisationStub = 78 // VERSION for 2.0.6 released
 
-    const val FilePath = 76 // VERSION for 2.0.6
-    const val FileLocale = 76 // VERSION for 2.0.6
-    const val ConfigSymbol = 76 // VERSION for 2.0.6
-    const val Define = 76 // VERSION for 2.0.6
-    const val Merged = 76 // VERSION for 2.0.6
+    const val FilePath = 78 // VERSION for 2.0.6 released
+    const val FileLocale = 78 // VERSION for 2.0.6 released
+    const val ConfigSymbol = 78 // VERSION for 2.0.6 released
+    const val Define = 78 // VERSION for 2.0.6 released
+    const val Merged = 78 // VERSION for 2.0.6 released
 }
