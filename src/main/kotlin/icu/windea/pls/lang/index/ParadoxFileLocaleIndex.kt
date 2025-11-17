4c9e1cd58967cb579dee5c9a453c9db8ffad54d9
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-11-17 16:55:47 +0800

    索引优化：`ParadoxFileLocaleIndex` - 如果可行，使用 LighterAST

--- src/main/kotlin/icu/windea/pls/lang/index/ParadoxFileLocaleIndex.kt
+++ src/main/kotlin/icu/windea/pls/lang/index/ParadoxFileLocaleIndex.kt
@@ -4,11 +4,13 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.DataIndexer
 import com.intellij.util.indexing.FileBasedIndex
 import com.intellij.util.indexing.FileContent
+import com.intellij.util.indexing.PsiDependentFileContent
 import com.intellij.util.indexing.ScalarIndexExtension
 import com.intellij.util.io.EnumeratorStringDescriptor
-import icu.windea.pls.core.castOrNull
+import icu.windea.pls.core.isNotNullOrEmpty
 import icu.windea.pls.localisation.ParadoxLocalisationFileType
 import icu.windea.pls.localisation.psi.ParadoxLocalisationFile
+import icu.windea.pls.localisation.psi.ParadoxLocalisationLightTreeUtil
 import java.util.*
 
 /**
@@ -33,11 +35,21 @@ class ParadoxFileLocaleIndex : ScalarIndexExtension<String>() {
 
     override fun getKeyDescriptor() = keyDescriptor
 
-    private fun indexData(inputData: FileContent): Map<String?, Void?> {
-        val locale = when (inputData.fileType) {
-            is ParadoxLocalisationFileType -> inputData.psiFile.castOrNull<ParadoxLocalisationFile>()?.propertyList?.locale?.name.orEmpty()
-            else -> ""
+    private fun indexData(inputData: FileContent): Map<String, Void?> {
+        if (inputData.fileType !is ParadoxLocalisationFileType) return emptyMap()
+        if (inputData is PsiDependentFileContent) {
+            // use lighter AST if possible to optimize performance
+            val tree = inputData.lighterAST
+            val locale = ParadoxLocalisationLightTreeUtil.getLocaleFromRootNode(tree)
+            if (locale.isNotNullOrEmpty()) return emptyMap()
+            return Collections.singletonMap(locale, null)
+        } else {
+            // fallback to PSI
+            val psiFile = inputData.psiFile
+            if (psiFile !is ParadoxLocalisationFile) return emptyMap()
+            val locale = psiFile.propertyList?.locale?.name
+            if (locale.isNullOrEmpty()) return emptyMap()
+            return Collections.singletonMap(locale, null)
         }
-        return Collections.singletonMap(locale, null)
     }
 }
--- src/main/kotlin/icu/windea/pls/localisation/psi/ParadoxLocalisationLightTreeUtil.kt
+++ src/main/kotlin/icu/windea/pls/localisation/psi/ParadoxLocalisationLightTreeUtil.kt
@@ -3,11 +3,18 @@ package icu.windea.pls.localisation.psi
 import com.intellij.lang.LighterAST
 import com.intellij.lang.LighterASTNode
 import icu.windea.pls.core.children
+import icu.windea.pls.core.childrenOfType
 import icu.windea.pls.core.firstChild
 import icu.windea.pls.core.internNode
 import icu.windea.pls.localisation.psi.ParadoxLocalisationElementTypes.*
 
 object ParadoxLocalisationLightTreeUtil {
+    fun getLocaleFromRootNode(tree: LighterAST): String? {
+        val rootNode = tree.root
+        val propertyListNode = rootNode.childrenOfType(tree, PROPERTY_LIST).singleOrNull() ?: return null
+        return getLocaleFromPropertyListNode(propertyListNode, tree)
+    }
+
     fun getLocaleFromPropertyListNode(node: LighterASTNode, tree: LighterAST): String? {
         return node.firstChild(tree, LOCALE)
             ?.firstChild(tree, LOCALE_TOKEN)
