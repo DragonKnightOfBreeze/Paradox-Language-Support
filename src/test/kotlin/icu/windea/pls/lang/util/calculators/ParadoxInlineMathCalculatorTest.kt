2b0c0ca1ce4a2878d33a6d75247556e2b55431b3
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-12-21 01:04:42 +0800

    支持通过意向动作，根据传参信息计算内联数学表达式 - s3 补充更多测试

--- src/test/kotlin/icu/windea/pls/lang/util/calculators/ParadoxInlineMathCalculatorTest.kt
+++ src/test/kotlin/icu/windea/pls/lang/util/calculators/ParadoxInlineMathCalculatorTest.kt
@@ -60,6 +60,85 @@ class ParadoxInlineMathCalculatorTest : BasePlatformTestCase() {
     }
 
     @Test
+    fun complex() {
+        myFixture.configureByFile("features/calculators/calculator_complex.test.txt")
+        val file = myFixture.file as ParadoxScriptFile
+        val map = toInlineMathMap(file)
+        val calculator = ParadoxInlineMathCalculator()
+
+        assertResult(5) { calculator.calculate(map.getValue("k1")) }
+        assertResult(21) { calculator.calculate(map.getValue("k2")) }
+        assertResult(3) { calculator.calculate(map.getValue("k3")) }
+        assertResult(3) { calculator.calculate(map.getValue("k4")) }
+        assertResult(3) { calculator.calculate(map.getValue("k5")) }
+        assertResult(4) { calculator.calculate(map.getValue("k6")) }
+        assertResult(2.5f) { calculator.calculate(map.getValue("k7")) }
+        assertResult(3) { calculator.calculate(map.getValue("k8")) }
+        assertResult(7) { calculator.calculate(map.getValue("k9")) }
+    }
+
+    @Test
+    fun paramsEdge() {
+        myFixture.configureByFile("features/calculators/calculator_params_edge.test.txt")
+        val file = myFixture.file as ParadoxScriptFile
+        val map = toInlineMathMap(file)
+        val calculator = ParadoxInlineMathCalculator()
+
+        assertResult(2) { calculator.calculate(map.getValue("k1")) }
+        assertResult(3) { calculator.calculate(map.getValue("k2")) }
+        assertResult(3) { calculator.calculate(map.getValue("k3")) }
+        assertResult(IllegalArgumentException::class.java) { calculator.calculate(map.getValue("k3"), mapOf("\$NUM$" to "1")) }
+        assertResult(30) { calculator.calculate(map.getValue("k3"), mapOf("\$NUM|1$" to "10", "\$NUM|2$" to "20")) }
+
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k4")) }
+
+        assertResult(3) { calculator.calculate(map.getValue("k5"), mapOf("\$NUM|1$" to "2")) }
+        assertResult(3) { calculator.calculate(map.getValue("k6"), mapOf("\$NUM$" to "2")) }
+
+        assertResult(IllegalArgumentException::class.java) { calculator.calculate(map.getValue("k7")) }
+        assertResult(IllegalArgumentException::class.java) { calculator.calculate(map.getValue("k7"), mapOf("\$MISS$" to "NaN")) }
+    }
+
+    @Test
+    fun errors() {
+        myFixture.configureByFile("features/calculators/calculator_errors.test.txt")
+        val file = myFixture.file as ParadoxScriptFile
+        val map = toInlineMathMap(file)
+        val calculator = ParadoxInlineMathCalculator()
+
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k1")) }
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k2")) }
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k3")) }
+        assertResult(ArithmeticException::class.java) { calculator.calculate(map.getValue("k4")) }
+        assertResult(ArithmeticException::class.java) { calculator.calculate(map.getValue("k5")) }
+    }
+
+    @Test
+    fun edgeCases() {
+        myFixture.configureByFile("features/calculators/calculator_edge_cases.test.txt")
+        val file = myFixture.file as ParadoxScriptFile
+        val map = toInlineMathMap(file)
+        val calculator = ParadoxInlineMathCalculator()
+
+        assertResult(9) { calculator.calculate(map.getValue("k1")) }
+        assertResult(9) { calculator.calculate(map.getValue("k2")) }
+        assertResult(2) { calculator.calculate(map.getValue("k3")) }
+        assertResult(8) { calculator.calculate(map.getValue("k4")) }
+
+        assertResult(IllegalArgumentException::class.java) { calculator.calculate(map.getValue("k5")) }
+        assertResult(3) { calculator.calculate(map.getValue("k5"), mapOf("\$NUM$" to "2")) }
+
+        assertResult(2) { calculator.calculate(map.getValue("k6"), mapOf("\$NUM$" to "3")) }
+        assertResult(1.5f) { calculator.calculate(map.getValue("k6"), mapOf("\$NUM$" to "2")) }
+
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k7")) }
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k8")) }
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k9")) }
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k10")) }
+        assertResult(IllegalStateException::class.java) { calculator.calculate(map.getValue("k11")) }
+    }
+
+    @Test
     fun simpleWithSv() {
         myFixture.configureByFile("features/calculators/calculator_simple_with_sv.test.txt")
         val file = myFixture.file as ParadoxScriptFile
