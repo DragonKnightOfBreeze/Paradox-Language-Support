6ab95b9d1c41bf0efe07039d834b23a4e9c6bf40
DragonKnightOfBreeze <dk_breeze@qq.com>
2025-12-21 22:06:11 +0800

    移除 `CwtInheritConfigPostProcessor`（目前并未使用）

--- src/main/kotlin/icu/windea/pls/ep/config/CwtConfigPostProcessors.kt
+++ src/main/kotlin/icu/windea/pls/ep/config/CwtConfigPostProcessors.kt
@@ -21,44 +21,6 @@ class CwtBaseConfigPostProcessor : CwtConfigPostProcessor {
     }
 }
 
-class CwtInheritConfigPostProcessor : CwtConfigPostProcessor {
-    override fun postProcess(config: CwtMemberConfig<*>) {
-        // TODO 1.3.18+
-        // val configGroup = config.configGroup
-        //
-        // var inheritConfigsValue: String? = null
-        // var inheritOptionsValue: String? = null
-        // var inheritDocValue: String? = null
-        //
-        // val oldOptions = mutableListOf<CwtOptionMemberConfig<*>>()
-        // config.options?.forEach { o ->
-        //    when(o){
-        //        is CwtOptionConfig -> when(o.key) {
-        //            "inherit_configs" -> o.stringValue?.let { inheritConfigsValue = it }
-        //            "inherit_options" -> o.stringValue?.let { inheritOptionsValue = it }
-        //            "inherit_doc" -> o.stringValue?.let { inheritDocValue = it }
-        //            else -> oldOptions += o
-        //        }
-        //        is CwtOptionValueConfig -> oldOptions += o
-        //    }
-        // }
-        //
-        // if(inheritConfigsValue == null && inheritOptionsValue == null && inheritDocValue == null) return config
-        //
-        // var newConfigs: List<CwtMemberConfig<*>>? = null
-        // var newOptions: List<CwtOptionMemberConfig<*>>? = null
-        // var newDocumentation: String? = null
-        //
-        // inheritDocValue?.let { pathExpression ->
-        //    CwtConfigManager.getConfigByPathExpression(configGroup, pathExpression)?.let { newConfig ->
-        //        newDocumentation = newConfig.documentation
-        //    }
-        // }
-        //
-        // if(newConfigs == null && newOptions == null && newDocumentation == null) return config
-    }
-}
-
 class CwtInjectConfigPostProcessor : CwtConfigPostProcessor {
     override fun supports(config: CwtMemberConfig<*>): Boolean {
         val pathExpression = config.optionData { inject } ?: return false
