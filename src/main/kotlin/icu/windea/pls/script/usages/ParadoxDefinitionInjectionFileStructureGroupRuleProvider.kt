767b821a316e6700b3db1d2786409467c4159ebb
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-01-01 20:50:31 +0800

    #252 新功能：支持 VIC3 和 EU5 中的定义注入特性 - 更多兼容

--- src/main/kotlin/icu/windea/pls/script/usages/ParadoxDefinitionInjectionFileStructureGroupRuleProvider.kt
+++ src/main/kotlin/icu/windea/pls/script/usages/ParadoxDefinitionInjectionFileStructureGroupRuleProvider.kt
@@ -1,5 +1,10 @@
 package icu.windea.pls.script.usages
 
+import com.intellij.openapi.project.Project
+import com.intellij.usages.UsageViewSettings
+import com.intellij.usages.impl.FileStructureGroupRuleProvider
+import com.intellij.usages.rules.UsageGroupingRule
+
 /**
  * 文件结构支持 - 定义注入的用法分组规则。
  */
