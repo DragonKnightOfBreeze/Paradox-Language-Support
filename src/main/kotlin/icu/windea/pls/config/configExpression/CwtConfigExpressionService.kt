d022d695ce091513884cddd5a198f466d99d9d59
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-01-17 22:35:24 +0800

    性能优化：`CwtConfigExpressionService.getPriority` - 回退缓存（没什么效果）

--- src/main/kotlin/icu/windea/pls/config/configExpression/CwtConfigExpressionService.kt
+++ src/main/kotlin/icu/windea/pls/config/configExpression/CwtConfigExpressionService.kt
@@ -1,21 +1,12 @@
 package icu.windea.pls.config.configExpression
 
 import icu.windea.pls.config.configGroup.CwtConfigGroup
-import icu.windea.pls.core.cache.CacheBuilder
-import icu.windea.pls.core.cache.cancelable
-import icu.windea.pls.core.util.getValue
-import icu.windea.pls.core.util.provideDelegate
-import icu.windea.pls.core.util.registerKey
 import icu.windea.pls.ep.config.configExpression.CwtDataExpressionMerger
 import icu.windea.pls.ep.config.configExpression.CwtDataExpressionPriorityProvider
 import icu.windea.pls.ep.config.configExpression.CwtDataExpressionResolver
 import icu.windea.pls.ep.config.configExpression.CwtRuleBasedDataExpressionResolver
 
 object CwtConfigExpressionService {
-    private val CwtConfigGroup.dataExpressionPriorityCache by registerKey(CwtConfigGroup.Keys) {
-        CacheBuilder("expireAfterAccess=30m").build<String, Double>().cancelable()
-    }
-
     val allRules by lazy {
         CwtDataExpressionResolver.EP_NAME.extensionList.filterIsInstance<CwtRuleBasedDataExpressionResolver>().flatMap { it.rules }
     }
@@ -59,11 +50,6 @@ object CwtConfigExpressionService {
      * @see CwtDataExpressionPriorityProvider.getPriority
      */
     fun getPriority(configExpression: CwtDataExpression, configGroup: CwtConfigGroup): Double {
-        val cache = configGroup.dataExpressionPriorityCache
-        return cache.get(configExpression.expressionString) { doGetPriority(configExpression, configGroup) }
-    }
-
-    private fun doGetPriority(configExpression: CwtDataExpression, configGroup: CwtConfigGroup): Double {
         CwtDataExpressionPriorityProvider.EP_NAME.extensionList.forEach f@{ ep ->
             val r = ep.getPriority(configExpression, configGroup)
             if (r > 0) return r
