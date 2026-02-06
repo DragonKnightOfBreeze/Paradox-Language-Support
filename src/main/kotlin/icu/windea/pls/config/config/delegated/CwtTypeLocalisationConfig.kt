0fb1fdabcc0d7c3472f7c5fa7aa88a9ae4af03e0
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-02-06 23:14:28 +0800

    重构各种存在解析逻辑的领域模型，将接口代码和实现代码放到同一文件中，便于维护

--- src/main/kotlin/icu/windea/pls/config/config/delegated/CwtTypeLocalisationConfig.kt
+++ src/main/kotlin/icu/windea/pls/config/config/delegated/CwtTypeLocalisationConfig.kt
@@ -1,11 +1,17 @@
 package icu.windea.pls.config.config.delegated
 
+import com.intellij.openapi.util.UserDataHolderBase
 import icu.windea.pls.config.config.CwtDelegatedConfig
 import icu.windea.pls.config.config.CwtPropertyConfig
-import icu.windea.pls.config.config.delegated.impl.CwtTypeLocalisationConfigResolverImpl
 import icu.windea.pls.config.configExpression.CwtLocalisationLocationExpression
 import icu.windea.pls.config.configExpression.CwtLocationExpression
+import icu.windea.pls.config.util.CwtConfigResolverScope
+import icu.windea.pls.core.cache.CacheBuilder
+import icu.windea.pls.core.cache.cancelable
+import icu.windea.pls.core.optimized
+import icu.windea.pls.core.removeSurroundingOrNull
 import icu.windea.pls.cwt.psi.CwtProperty
+import icu.windea.pls.lang.resolve.expression.ParadoxDefinitionSubtypeExpression
 
 /**
  * 类型本地化规则。
@@ -52,3 +58,55 @@ interface CwtTypeLocalisationConfig : CwtDelegatedConfig<CwtProperty, CwtPropert
 
     companion object : Resolver by CwtTypeLocalisationConfigResolverImpl()
 }
+
+// region Implementations
+
+private class CwtTypeLocalisationConfigResolverImpl : CwtTypeLocalisationConfig.Resolver, CwtConfigResolverScope {
+    // no logger here (unnecessary)
+
+    override fun resolve(config: CwtPropertyConfig): CwtTypeLocalisationConfig? = doResolve(config)
+
+    private fun doResolve(config: CwtPropertyConfig): CwtTypeLocalisationConfig? {
+        val locationConfigs: MutableList<Pair<String?, CwtLocationConfig>> = mutableListOf()
+        val props1 = config.properties ?: return null
+        for (prop1 in props1) {
+            val subtypeName = prop1.key.removeSurroundingOrNull("subtype[", "]")?.optimized()
+            if (subtypeName != null) {
+                val props2 = prop1.properties ?: continue
+                for (prop2 in props2) {
+                    val locationConfig = CwtLocationConfig.resolve(prop2) ?: continue
+                    locationConfigs.add(subtypeName to locationConfig)
+                }
+            } else {
+                val locationConfig = CwtLocationConfig.resolve(prop1) ?: continue
+                locationConfigs.add(null to locationConfig)
+            }
+        }
+        return CwtTypeLocalisationConfigImpl(config, locationConfigs.optimized())
+    }
+
+}
+
+private class CwtTypeLocalisationConfigImpl(
+    override val config: CwtPropertyConfig,
+    override val locationConfigs: List<Pair<String?, CwtLocationConfig>> // (subtypeExpression, locationConfig)
+) : UserDataHolderBase(), CwtTypeLocalisationConfig {
+    private val configsCache = CacheBuilder().build<String, List<CwtLocationConfig>>().cancelable()
+
+    override fun getConfigs(subtypes: List<String>): List<CwtLocationConfig> {
+        val cacheKey = subtypes.joinToString(",")
+        return configsCache.get(cacheKey) {
+            val result = mutableListOf<CwtLocationConfig>()
+            for ((subtypeExpression, locationConfig) in locationConfigs) {
+                if (subtypeExpression == null || ParadoxDefinitionSubtypeExpression.resolve(subtypeExpression).matches(subtypes)) {
+                    result.add(locationConfig)
+                }
+            }
+            result
+        }
+    }
+
+    override fun toString() = "CwtTypeLocalisationConfigImpl(locationConfigs=$locationConfigs)"
+}
+
+// endregion
--- src/main/kotlin/icu/windea/pls/config/config/delegated/impl/CwtLocationConfigResolverImpl.kt
+++
@@ -1,7 +0,0 @@
-package icu.windea.pls.config.config.delegated.impl
-
-import com.intellij.openapi.util.UserDataHolderBase
-import icu.windea.pls.config.config.CwtPropertyConfig
-import icu.windea.pls.config.config.delegated.CwtLocationConfig
-import icu.windea.pls.config.config.stringValue
-import icu.windea.pls.config.util.CwtConfigResolverScope
