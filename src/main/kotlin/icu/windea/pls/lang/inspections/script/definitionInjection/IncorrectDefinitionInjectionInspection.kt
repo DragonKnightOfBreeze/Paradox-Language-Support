e134e28fe38be39af817b27fd9b1050ea23638ee
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-03-12 00:56:23 +0800

    Optimize `IncorrectDefinitionInjectionInspection`

--- src/main/kotlin/icu/windea/pls/lang/inspections/script/definitionInjection/IncorrectDefinitionInjectionInspection.kt
+++ src/main/kotlin/icu/windea/pls/lang/inspections/script/definitionInjection/IncorrectDefinitionInjectionInspection.kt
@@ -2,12 +2,14 @@ package icu.windea.pls.lang.inspections.script.definitionInjection
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.DumbService
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.ui.dsl.builder.*
 import icu.windea.pls.PlsBundle
 import icu.windea.pls.core.toAtomicProperty
 import icu.windea.pls.lang.inspections.PlsInspectionUtil
 import icu.windea.pls.lang.util.ParadoxDefinitionInjectionManager
+import icu.windea.pls.model.ParadoxDefinitionInjectionInfo
 import icu.windea.pls.script.psi.ParadoxScriptProperty
 import icu.windea.pls.script.psi.ParadoxScriptVisitor
 import javax.swing.JComponent
@@ -32,23 +34,25 @@ class IncorrectDefinitionInjectionInspection : DefinitionInjectionInspectionBase
                 }
                 if (definitionInjectionInfo.type.isNullOrEmpty()) return // considered "unsupported"
                 if (definitionInjectionInfo.typeConfig == null) return // considered "unsupported"
-                val target = definitionInjectionInfo.target
-                val type = definitionInjectionInfo.type
-                val relax = definitionInjectionInfo.isRelaxMode()
-                val targetExist = definitionInjectionInfo.isTargetExist(holder.file)
-                if (!targetExist) {
-                    if (relax) {
-                        if (checkForRelaxModes) {
-                            val description = PlsBundle.message("inspection.script.incorrectDefinitionInjection.desc.3", target, type)
-                            val highlightType = PlsInspectionUtil.getWeakerHighlightType() // use weaker highlight type
-                            holder.registerProblem(element.propertyKey, description, highlightType)
-                        }
-                    } else {
-                        val description = PlsBundle.message("inspection.script.incorrectDefinitionInjection.desc.2", target, type)
-                        holder.registerProblem(element.propertyKey, description)
-                    }
-                }
+                checkTargetExistence(element, definitionInjectionInfo, holder)
             }
+        }
+    }
+
+    private fun checkTargetExistence(element: ParadoxScriptProperty, definitionInjectionInfo: ParadoxDefinitionInjectionInfo, holder: ProblemsHolder) {
+        val target = definitionInjectionInfo.target ?: return
+        val type = definitionInjectionInfo.type ?: return
+        if (DumbService.isDumb(holder.project)) return // ignore in dumb mode
+        if (definitionInjectionInfo.isTargetExist(holder.file)) return
+        if (definitionInjectionInfo.isRelaxMode()) {
+            if (checkForRelaxModes) {
+                val description = PlsBundle.message("inspection.script.incorrectDefinitionInjection.desc.3", target, type)
+                val highlightType = PlsInspectionUtil.getWeakerHighlightType() // use weaker highlight type
+                holder.registerProblem(element.propertyKey, description, highlightType)
+            }
+        } else {
+            val description = PlsBundle.message("inspection.script.incorrectDefinitionInjection.desc.2", target, type)
+            holder.registerProblem(element.propertyKey, description)
         }
     }
 
