89f492ca6b89485d8fbb1ac1728fe2b090a21a91
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-06-16 00:10:41 +0800

    Update index versions

--- src/main/kotlin/icu/windea/pls/lang/index/PlsIndexVersions.kt
+++ src/main/kotlin/icu/windea/pls/lang/index/PlsIndexVersions.kt
@@ -1,15 +1,15 @@
 package icu.windea.pls.lang.index
 
 object PlsIndexVersions {
-    const val ScriptStub = 93 // 2.1.8
-    const val LocalisationStub = 93 // 2.1.8
+    const val ScriptStub = 94 // 2.1.10
+    const val LocalisationStub = 94 // 2.1.10
 
-    const val FilePath = 93 // 2.1.8
-    const val FileLocale = 93 // 2.1.8
-    const val IncludedDirectory = 93 // 2.1.8
-    const val ConfigSymbol = 93 // 2.1.8
-    const val ComplexEnumValue = 93 // 2.1.8
-    const val Definition = 93 // 2.1.8
-    const val DefinitionInjection = 93 // 2.1.8
-    const val Merged = 94 // 2.1.9
+    const val FilePath = 94 // 2.1.10
+    const val FileLocale = 94 // 2.1.10
+    const val IncludedDirectory = 94 // 2.1.10
+    const val ConfigSymbol = 94 // 2.1.10
+    const val ComplexEnumValue = 94 // 2.1.10
+    const val Definition = 94 // 2.1.10
+    const val DefinitionInjection = 94 // 2.1.10
+    const val Merged = 94 // 2.1.10
 }
