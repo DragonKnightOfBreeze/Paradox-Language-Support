35fb523f8b6b55d69984e62b7f4ebc4e3ef5c14d
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-06-16 16:48:13 +0800

    Update index version (script file grammar changed)

--- src/main/kotlin/icu/windea/pls/lang/index/PlsIndexVersions.kt
+++ src/main/kotlin/icu/windea/pls/lang/index/PlsIndexVersions.kt
@@ -1,15 +1,15 @@
 package icu.windea.pls.lang.index
 
 object PlsIndexVersions {
-    const val ScriptStub = 94 // 2.1.10
-    const val LocalisationStub = 94 // 2.1.10
+    const val ScriptStub = 95 // 2.1.10
+    const val LocalisationStub = 95 // 2.1.10
 
-    const val FilePath = 94 // 2.1.10
-    const val FileLocale = 94 // 2.1.10
-    const val IncludedDirectory = 94 // 2.1.10
-    const val ConfigSymbol = 94 // 2.1.10
-    const val ComplexEnumValue = 94 // 2.1.10
-    const val Definition = 94 // 2.1.10
-    const val DefinitionInjection = 94 // 2.1.10
-    const val Merged = 94 // 2.1.10
+    const val FilePath = 95 // 2.1.10
+    const val FileLocale = 95 // 2.1.10
+    const val IncludedDirectory = 95 // 2.1.10
+    const val ConfigSymbol = 95 // 2.1.10
+    const val ComplexEnumValue = 95 // 2.1.10
+    const val Definition = 95 // 2.1.10
+    const val DefinitionInjection = 95 // 2.1.10
+    const val Merged = 95 // 2.1.10
 }
