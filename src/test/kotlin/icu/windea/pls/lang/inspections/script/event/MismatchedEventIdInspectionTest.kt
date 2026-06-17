b78012fc5482a38f8cb7250e3b27a72a6b5a62c5
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-06-17 17:45:46 +0800

    Add tests for event inspections

---
+++ src/test/kotlin/icu/windea/pls/lang/inspections/script/event/MismatchedEventIdInspectionTest.kt
@@ -0,0 +1,202 @@
+package icu.windea.pls.lang.inspections.script.event
+
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import icu.windea.pls.PlsBundle
+import icu.windea.pls.model.ParadoxGameType
+import icu.windea.pls.test.InspectionTestScope
+import icu.windea.pls.test.clearIntegrationTest
+import icu.windea.pls.test.initConfigGroups
+import icu.windea.pls.test.markFileInfo
+import icu.windea.pls.test.markIntegrationTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+@TestDataPath("\$CONTENT_ROOT/testData")
+class MismatchedEventIdInspectionTest : BasePlatformTestCase(), InspectionTestScope {
+    override fun getTestDataPath() = "src/test/testData"
+
+    @Before
+    fun doSetUp() {
+        markIntegrationTest()
+        initConfigGroups(project, ParadoxGameType.Stellaris, ParadoxGameType.Eu5)
+        myFixture.enableInspections(MismatchedEventIdInspection::class.java)
+    }
+
+    @After
+    fun doTearDown() = clearIntegrationTest()
+
+    // region Sequential (Stellaris) - Matched
+
+    @Test
+    fun stellaris_matchedSimple() {
+        markFileInfo(ParadoxGameType.Stellaris, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            event = {
+                id = foo.1
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun stellaris_matchedMultipleEvents() {
+        markFileInfo(ParadoxGameType.Stellaris, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            event = {
+                id = foo.1
+            }
+            event = {
+                id = foo.2
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun stellaris_matchedNamespaceSwitch() {
+        markFileInfo(ParadoxGameType.Stellaris, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            event = {
+                id = foo.1
+            }
+            namespace = bar
+            event = {
+                id = bar.1
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    // endregion
+
+    // region Sequential (Stellaris) - Mismatched (desc.2)
+
+    @Test
+    fun stellaris_mismatchedSingle() {
+        val eventId = "bar.1"
+        val namespace = "foo"
+        val desc = PlsBundle.message("inspection.script.mismatchedEventId.desc.2", eventId, namespace)
+        val tag = desc.toWarningTag()
+        markFileInfo(ParadoxGameType.Stellaris, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            event = {
+                id = ${tag.start}bar.1${tag.end}
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun stellaris_mismatchedAfterSwitch() {
+        val eventId = "foo.2"
+        val namespace = "bar"
+        val desc = PlsBundle.message("inspection.script.mismatchedEventId.desc.2", eventId, namespace)
+        val tag = desc.toWarningTag()
+        markFileInfo(ParadoxGameType.Stellaris, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            event = {
+                id = foo.1
+            }
+            namespace = bar
+            event = {
+                id = ${tag.start}foo.2${tag.end}
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    // endregion
+
+    // region Sequential (Stellaris) - No Binding (desc.1)
+
+    @Test
+    fun stellaris_noNamespace() {
+        val eventId = "foo.1"
+        val desc = PlsBundle.message("inspection.script.mismatchedEventId.desc.1", eventId)
+        val tag = desc.toWarningTag()
+        markFileInfo(ParadoxGameType.Stellaris, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            event = {
+                id = ${tag.start}foo.1${tag.end}
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    // endregion
+
+    // region Mixed (EU5) - Matched
+
+    @Test
+    fun eu5_matchedSimple() {
+        markFileInfo(ParadoxGameType.Eu5, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            foo.1 = {
+                type = country_event
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun eu5_matchedMultipleNamespaces() {
+        markFileInfo(ParadoxGameType.Eu5, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            namespace = bar
+            foo.1 = {
+                type = country_event
+            }
+            bar.1 = {
+                type = country_event
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    // endregion
+
+    // region Mixed (EU5) - No Binding (desc.1)
+
+    @Test
+    fun eu5_unboundNoMatchingNamespace() {
+        val eventId = "bar.1"
+        val desc = PlsBundle.message("inspection.script.mismatchedEventId.desc.1", eventId)
+        val tag = desc.toWarningTag()
+        markFileInfo(ParadoxGameType.Eu5, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            namespace = foo
+            ${tag.start}bar.1${tag.end} = {
+                type = country_event
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun eu5_unboundNoNamespace() {
+        val eventId = "foo.1"
+        val desc = PlsBundle.message("inspection.script.mismatchedEventId.desc.1", eventId)
+        val tag = desc.toWarningTag()
+        markFileInfo(ParadoxGameType.Eu5, "events/test_events.txt")
+        myFixture.configureByText("test_events.txt", """
+            ${tag.start}foo.1${tag.end} = {
+                type = country_event
+            }
+        """.trimIndent())
+        myFixture.checkHighlighting()
+    }
+
+    // endregion
+}
