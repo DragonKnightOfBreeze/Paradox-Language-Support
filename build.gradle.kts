f7c6418a2fe81d14b74b8cef630c83f82f9f10d0
DragonKnightOfBreeze <dk_breeze@qq.com>
2026-07-14 01:44:50 +0800

    Fix `Indexing timeout` during tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,11 @@ buildscript {
 }
 
 plugins {
+    // NOTE 3.0.0 [compatibility] DO NOT update `org.jetbrains.intellij.platform` to 2.18.0+ - may cause `Indexing timeout` during tests
+    // https://github.com/JetBrains/intellij-platform-gradle-plugin/issues/2188
+
     id("org.jetbrains.kotlin.jvm") version "2.1.20" // https://kotlinlang.org/docs/gradle.html
-    id("org.jetbrains.intellij.platform") version "2.18.0" // https://github.com/JetBrains/intellij-platform-gradle-plugin
+    id("org.jetbrains.intellij.platform") version "2.17.0" // https://github.com/JetBrains/intellij-platform-gradle-plugin
     id("org.jetbrains.grammarkit") version "2023.3.0.3" // https://github.com/JetBrains/gradle-grammar-kit-plugin
     id("org.jetbrains.kotlinx.kover") version "0.9.8" // https://github.com/Kotlin/kotlinx-kover
     // id("org.jetbrains.changelog") version "2.5.0" // https://github.com/JetBrains/gradle-changelog-plugin
